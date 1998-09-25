a76e19d2186a80178f7feaff6f6c092fa321ee55
Simon Michael <>
1998-09-25 23:50:32 +0000

    Fixed a typo and and added the #define MSDOS_MAGIC.

--- elks/include/linuxmt/msdos.h
+++ elks/include/linuxmt/msdos.h
@@ -8,10 +8,13 @@
 #ifndef EXEC_HEADER_SIZE
 #define EXEC_HEADER_SIZE	32
 #endif
+#ifndef MSDOS_MAGIC
+#define MSDOS_MAGIC		0x4d5a /* MZ */
+#endif
 
 struct msdos_exec_hdr
 {
 	unsigned int magic; /* FIXME - is int 16bit? */
-}
+};
 
 #endif
