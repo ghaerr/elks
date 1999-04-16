187d941ef671df80a3e8d717acfa06cceadd5d50
Andrew Richards <>
1999-04-16 09:52:09 +0000

    Missing header file for developing shared library support.

---
+++ elks/include/linuxmt/dll.h
@@ -0,0 +1,20 @@
+#ifndef __LINUXMT_DLL_H
+#define __LINUXMT_DLL_H
+
+/*
+ *	DLL structures
+ */
+
+#define MAX_DLLS	8
+
+#define DLL_USED	1
+#define DLL_UNUSED	0
+
+struct dll_entry
+{
+	int		d_state;
+	struct inode *	d_inode;
+	unsigned short	d_cseg;
+};
+
+#endif
