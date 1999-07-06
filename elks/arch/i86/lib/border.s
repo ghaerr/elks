84d9a738455fe0ea739f0181f61ef41f230dc7cf
Andrew Richards <>
1999-07-06 18:20:23 +0000

    Added byte order functions for romfs.

---
+++ elks/arch/i86/lib/border.s
@@ -0,0 +1,20 @@
+| border.s
+
+	.globl _ntohl
+	.globl _htonl
+	.text
+	.even
+
+_htonl:
+_ntohl:
+	push	bp
+	mov	bp,sp
+	mov     bx,4[bp]
+	mov     ax,6[bp]
+	mov	cl,#8
+	ror	ax,cl
+	ror	bx,cl
+	mov	dx,bx
+	pop	bp
+	ret
+
---
+++ elks/include/arch/border.h
@@ -0,0 +1,7 @@
+#ifndef _ARCH_BORDER_H
+#define _ARCH_BORDER_H
+
+extern unsigned long htonl();
+extern unsigned long ntohl();
+
+#endif _ARCH_BORDER_H
