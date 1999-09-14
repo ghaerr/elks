6f9f8ac29b2583f6009b3da36df67ecedda3a10e
Andrew Richards <>
1999-09-14 20:11:57 +0000

    Fix type of task->child_wait by Thomas McWilliams.

--- elks/include/linuxmt/sched.h
+++ elks/include/linuxmt/sched.h
@@ -65,7 +65,7 @@ struct task_struct
 	struct tty * tty;
 /*	__u8 link_count;		/* Symlink loop counter, now global */
 	struct task_struct *p_parent, *p_prevsib, *p_nextsib, *p_child;	 
-  	struct wait_queue child_wait;
+  	struct wait_queue * child_wait;
 	pid_t child_lastend;
 	int lastend_status;
 	struct inode * t_inode;
