7381bcbf317c81e16c16e048dde36d13dea409f4
Andrew Richards <>
1999-10-28 15:15:02 +0000

    Latest version of ramdisk tool to go with latest kernel driver.

--- elkscmd/disk_utils/ramdisk.c
+++ elkscmd/disk_utils/ramdisk.c
@@ -25,7 +25,7 @@ char **argv;
 		size = 64; /* default */
 	
 	if ((size < 1) || (size > MAX_SIZE)) {
-		fprintf(stderr, "ramdisk: invalid size; use integer in range of 1 .. %d, ramdisk will round it up to nearest multiple of 4 KB\n", MAX_SIZE);
+		fprintf(stderr, "ramdisk: invalid size; use integer in range of 1 .. %d\n", MAX_SIZE);
 		exit(1);
 	}
 	if (( fd = open(argv[1], 0) ) == -1) {
@@ -33,19 +33,11 @@ char **argv;
 		exit(1);
 	}
 	if (strcmp(argv[2],"make") == 0) {
-		/* recalculate size to # of 4 KB pages */
-			if ((size % 4) != 0) {
-				fprintf(stdout, "ramdisk: rounding size up to %d KB ...\n", ((size / 4) + 1) * 4);
-				size = size / 4 + 1;
-			} else {
-				size = size / 4;
-			}
-
 		if (ioctl(fd, RDCREATE, size)) {
 			perror("ramdisk");
 			exit(1);
 		}
-		fprintf(stdout,"ramdisk: %d KB ramdisk created on %s\n", size * 4, argv[1]);
+		fprintf(stdout,"ramdisk: %d KB ramdisk created on %s\n", size, argv[1]);
 		exit(0);
 	}
 	if (strcmp(argv[2],"kill") == 0) {
