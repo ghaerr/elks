6a15c44731eab95564fd6f32ea3d656da7c508b2
Andrew Richards <>
2000-01-19 14:01:25 +0000

    Fixes to ssd access code used in the wrt_dsk program.

--- elks/arch/i86/tools/wrt_disk.c
+++ elks/arch/i86/tools/wrt_disk.c
@@ -7,13 +7,49 @@
 
 /* NOTE: Each time this is run it erases the SSD */
 
+#define BLOCKS 2 /* set for 128K SSD, 1 Block = 64K */
+
 main()
 {
 	unsigned int high_address, low_address;
-	unsigned char data, not_done;
+	unsigned char data, result, not_done;
 
-	/* Comment to SSD in slot 'B' (under 'enter') */
+	/* Connect to SSD in slot 'B' (under 'enter') */
 	ssd_open4(0x01);
+	
+	/* pre-program all bits to zero */
+	LCD_Position(0,0);
+	LCD_WriteChar('P');
+	LCD_WriteChar('r');
+	LCD_WriteChar('e');
+	LCD_WriteChar('P');
+	LCD_WriteChar('r');
+	LCD_WriteChar('o');
+	LCD_WriteChar('g');
+
+	/* for block of 64K write data */
+	for (high_address = 0; high_address < BLOCKS ; high_address ++) {
+		low_address = 0;
+		not_done = 1;
+
+		while (not_done) {
+			if ( (low_address & 0x1F) == 0) {
+				LCD_Position(0,1);
+				print_address(high_address, low_address);
+			}
+
+			result = ssd_write4(high_address, low_address, 0);
+
+			if (result != data) {
+				LCD_Position(0,2);
+				error(data, result);
+			}
+
+			low_address ++;
+			if (low_address == 0) not_done = 0;	
+		}
+	}
+	
 
 	/* need to erase each chip in the SSD */
 	LCD_Position(0,0);
@@ -22,12 +58,26 @@ main()
 	LCD_WriteChar('a');
 	LCD_WriteChar('s');
 	LCD_WriteChar('e');
-	ssd_erase4(0);
+	LCD_WriteChar(' ');
+	LCD_WriteChar(' ');
+	
+	/* for block of 64K write data */
+	for (high_address = 0; high_address < BLOCKS ; high_address ++) {
+		LCD_Position(0,1);
+		print_address(high_address, 0);
+
+		result = ssd_erase4(0);
+
+		if ( result ) {
+			LCD_Position(0,2);
+			error(high_address, result);
+		}
+	}
 	
 	open_file();
 
 	/* lets create the disk image */
-	LCD_Position(0,1);
+	LCD_Position(0,0);
 	LCD_WriteChar('P');
 	LCD_WriteChar('r');
 	LCD_WriteChar('o');
@@ -37,17 +87,26 @@ main()
 	LCD_WriteChar('m');
 
 	/* for block of 64K write data */
-	for (high_address = 0; high_address < 2; high_address ++)
-	{
+	for (high_address = 0; high_address < BLOCKS ; high_address ++) {
 		low_address = 0;
 		not_done = 1;
 
-		while (not_done)
-		{
+		while (not_done) {
 			data = read_byte();
-			LCD_Position(0,2);
+			if ( (low_address & 0x1F) == 0) {
+				LCD_Position(0,1);
+				print_address(high_address, low_address);
+			}
+
 			LCD_WriteChar(data);
-			ssd_write4(high_address, low_address, data);
+
+			result = ssd_write4(high_address, low_address, data);
+
+			if (result != data) {
+				LCD_Position(0,2);
+				error(data, result);
+			}
+
 			low_address ++;
 			if (low_address == 0) not_done = 0;	
 		}
@@ -55,7 +114,7 @@ main()
 
 	close_file();
 
-	LCD_Position(0,3);
+	LCD_Position(0,2);
 	LCD_WriteChar('D');
 	LCD_WriteChar('o');
 	LCD_WriteChar('n');
@@ -67,3 +126,38 @@ main()
 
 	return(0);
 }
+
+int print_address(high, low)
+int high;
+int low;
+{
+	/* Print out the address at the current location */
+	LCD_WriteChar( '0' + ((high & 0xF000) >> 12));
+	LCD_WriteChar( '0' + ((high & 0x0F00) >> 8));
+	LCD_WriteChar( '0' + ((high & 0x00F0) >> 4));
+	LCD_WriteChar( '0' + (high & 0x000F) );
+	
+	LCD_WriteChar( '0' + ((low & 0xF000) >> 12));
+	LCD_WriteChar( '0' + ((low & 0x0F00) >> 8));
+	LCD_WriteChar( '0' + ((low & 0x00F0) >> 4));
+	LCD_WriteChar( '0' + (low & 0x000F) );
+
+	LCD_WriteChar( ' ' );
+}
+
+int error(data, result)
+int data;
+int result;
+{
+	LCD_WriteChar('E');
+	LCD_WriteChar('r');
+	LCD_WriteChar('r');
+	LCD_WriteChar('o');
+	LCD_WriteChar('r');
+
+	LCD_WriteChar(' ');
+	print_address(data, result);
+
+	while (1);
+}
+
