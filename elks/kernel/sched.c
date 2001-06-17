c06cb6e2965415c66b98356870cdbcf3ad4cf6b7
Alan Cox <>
2001-06-17 13:46:56 +0000

    Clean up

--- elks/kernel/sched.c
+++ elks/kernel/sched.c
@@ -16,17 +16,9 @@
 #include <linuxmt/types.h>
 #include <linuxmt/sched.h>
 #include <linuxmt/timer.h>
-#ifdef NEED_TQ
-#include <linuxmt/tqueue.h>
-#endif
 #include <arch/irq.h>
 
 #define init_task task[0]
-
-#ifdef ENDIS_BH
-int bh_mask_count[16];
-unsigned bh_mask = 0;
-#endif /* ENDIS_BH */
 
 __task task[MAX_TASKS];
 
@@ -42,46 +34,15 @@ static jiff_t lost_ticks_system = 0L;
 extern unsigned char can_tswitch;
 extern int lastirq;
 
-#ifdef NEED_TQ_TIMER
-DECLARE_TASK_QUEUE(tq_timer);
-#endif
-#ifdef NEED_IMM_BH
-DECLARE_TASK_QUEUE(tq_immediate);
-#endif
-#ifdef NEED_TQ_SCHEDULER
-DECLARE_TASK_QUEUE(tq_scheduler);
-#endif
-
 static void run_timer_list();
 static void run_old_timers();
 /* int need_resched = 0; */
 /* static int intr_count = 0; */
 
-#if 0
-/* This is a diagnostic dump of all currently running tasks... to be run after
- * fork() for now.  Not very intelligible yet */
-void print_tasks()
-{
-	int i;
-	__ptask pt;
-	printk("# pid state ds\n");
-	for (i = 0; i < MAX_TASKS; i++) {
-		pt = &task[i];
-		printk("%d %d %d %x %x\n", i, pt->pid, pt->state, pt->t_regs.cs, pt->t_regs.ds); 
-	}
-}
-#endif
-
 void add_to_runqueue(p)
 register struct task_struct * p;
 {
-#if 0   /* sanity tests */ /* This is always pre-checked */
-        if (p->next_run || p->prev_run) {
-                printk("task already on run-queue\n");
-                return;
-        }
-#endif
-#if 0
+#ifdef OLD_SCHED
         if (p->counter > current->counter + 3)
                 need_resched = 1;
 #endif
@@ -98,7 +59,7 @@ struct task_struct * p;
         register struct task_struct *next = p->next_run;
         register struct task_struct *prev = p->prev_run;
 
-#if 1   /* sanity tests */
+#if 0   /* sanity tests */
         if (!next || !prev) {
                 printk("task %d not on run-queue (state=%d)\n", p->pid,
 		       p->state);
@@ -121,7 +82,6 @@ static void process_timeout(__data)
 int __data;
 {
         register struct task_struct * p = (struct task_struct *) __data;
-
 /*	printk("process_timeout called!  data=%x, waking task %d\n", __data, p->pid); */
         p->timeout = 0UL;
         wake_up_process(p);
@@ -133,12 +93,6 @@ register struct task_struct * p;
 struct task_struct * prev;
 {
         int weight;
-
-	    /*
-	     * Realtime process, select the first one on the
-	     * runqueue (taking priorities within processes
-	     * into account).
-	     */
 
         /*
          * Give the process a first-approximation goodness value
@@ -185,32 +139,9 @@ void schedule()
 	if (currentp->state == TASK_EXITING)
 		return;
 
-/* intr_count is not used in ELKS anywhere else */
-/*	if (intr_count)
-		goto scheduling_in_interrupt; */
-
-#ifdef NEED_BH
-#ifdef ENDIS_BH
-	if (bh_active & bh_mask) {
-#else
-	if (bh_active) {
-#endif
-/*		intr_count = 1; */
-		do_bottom_half();
-/*		intr_count = 0; */
-	}
-#else
-#ifdef NEED_OLD_TIMERS
-	run_old_timers();
-#endif
 	run_timer_list();
-#endif
 
-#ifdef NEED_TQ_SCHEDULER
-	run_task_queue(&tq_scheduler);
-#endif
-
-#ifdef NEED_RSCHED
+#ifdef OLD_SCHED
 	need_resched = 0;
 #endif
 
@@ -302,7 +233,6 @@ void schedule()
 		current = next;
 
 		tswitch();	/* Won't return for a new task */
-/*		load_regs();	/* Returns to our caller */
 
 		if (timeout) {
 			del_timer(&timer);
@@ -311,8 +241,11 @@ void schedule()
 	return;
 
 scheduling_in_interrupt:
-        printk("Aiee: scheduling in interrupt %d - %d %d\n",
-                lastirq, currentp->pid, prev->pid);
+	/* Taking a timer IRQ during another IRQ or while in kernel space is
+	   quite legal. We just dont switch then */
+	if(lastirq > 0)
+	        printk("Aiee: scheduling in interrupt %d - %d %d\n",
+        	        lastirq, currentp->pid, prev->pid);
 }
 
 struct timer_list tl_list = {NULL,};
@@ -404,43 +337,6 @@ static void run_timer_list()
 	isti();
 }
 
-#ifdef NEED_OLD_TIMERS
-
-unsigned long timer_active = 0;
-struct timer_struct timer_table[32];
-
-static /*inline*/ void run_old_timers()
-{
-	register struct timer_struct *tp;
-	unsigned long mask;
-
-	for (mask = 1, tp = timer_table+0 ; mask ; tp++,mask += mask) {
-		if (mask > timer_active)
-			break;
-		if (!(mask & timer_active))
-			continue;
-		if (tp->expires > jiffies)
-			continue;
-		timer_active &= ~mask;
-		tp->fn();
-		isti();
-	}
-}
-#endif
-
-#ifdef NEED_TQ_TIMER
-void tqueue_bh()
-{
-	run_task_queue(&tq_timer);
-}
-#endif
-
-#ifdef NEED_IMM_BH
-void immediate_bh()
-{
-	run_task_queue(&tq_immediate);
-}
-#endif
 
 /* maybe someday I'll implement these profiling things -PL */
 #if 0
@@ -490,27 +386,12 @@ static void update_times()
 			p->counter -= ticks;
 			if (p->counter < 0) {
 				p->counter = 0;
-#ifdef NEED_RESCHED
 				need_resched = 1;
-#endif
 			}
 		}
         }
 }
 #endif /* OLD_SCHED */
-
-#ifdef NEED_BH
-static void timer_bh()
-{
-#ifdef OLD_SCHED
-	update_times();
-#endif /* OLD_SCHED */
-#ifdef NEED_OLD_TIMERS
-	run_old_timers();
-#endif
-	run_timer_list();
-}
-#endif
 
 void do_timer(regs)
 struct pt_regs * regs;
@@ -519,58 +400,10 @@ struct pt_regs * regs;
 #ifdef OLD_SCHED
         lost_ticks++;
 #endif /* OLD_SCHED */
-
-#ifdef NEED_BH
-        mark_bh(TIMER_BH);
-#endif
-
-#ifdef NEED_TQ_TIMER
-        if (tq_timer)
-                mark_bh(TQUEUE_BH);
-#endif
 }
-
-#ifdef NEED_BH
-void do_bottom_half()
-{
-        unsigned active;
-        unsigned mask, left;
-        void (**bh)();
-
-        isti();
-        bh = bh_base;
-#ifdef ENDIS_BH
-        active = bh_active & bh_mask;
-#else
-        active = bh_active;
-#endif
-        for (mask = 1, left = ~0 ; left & active ; bh++,mask += mask,left += left) {
-	  if (mask & active) {
-                        void (*fn)();
-                        bh_active &= ~mask;
-                        fn = *bh;
-                        if (!fn)
-                                goto bad_bh;
-                        fn();
-	  }
-        }
-        return;
-bad_bh:
-        printk ("irq.c:bad bottom half entry %08lx\n", mask);
-}
-#endif
 
 void sched_init()
 {
-#ifdef NEED_BH
-	init_bh(TIMER_BH, timer_bh);
-#endif
-#ifdef NEED_TQ_TIMER
-	init_bh(TQUEUE_BH, tqueue_bh);
-#endif
-#ifdef NEED_IMM_BH
-	init_bh(IMMEDIATE_BH, immediate_bh);
-#endif
 }
 
 
