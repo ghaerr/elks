41d03ca923d20049325cff2f037505af518174eb
Riley H. Williams <>
2002-01-15 00:23:01 +0000

    Updated fdisk's valid partition types to match Red Hat Linux 6.2's.

--- elkscmd/disk_utils/fdisk.c
+++ elkscmd/disk_utils/fdisk.c
@@ -33,165 +33,247 @@
 #define spc ((unsigned long)(geometry.heads*geometry.sectors))
 
 static Funcs funcs[] = {
-  { 'a',"Add partion",           add_part },
-  { 'b',"Set bootable flag",     set_boot },
-  { 'd',"Delete partition",      del_part },
-  { 'l',"List partition types",  list_types },
-  { 'p',"Print partition table", list_part },
-  { 'q',"Quit fdisk",            quit },
-  { 't',"Set partition type",    set_type },
-  { 'w',"Write partition table", write_out },
-  { '?',"Help",                  help },
-  {  0,  NULL,                   NULL }
+    { 'a',"Add partion",           add_part },
+    { 'b',"Set bootable flag",     set_boot },
+    { 'd',"Delete partition",      del_part },
+    { 'l',"List partition types",  list_types },
+    { 'p',"Print partition table", list_part },
+    { 'q',"Quit fdisk",            quit },
+    { 't',"Set partition type",    set_type },
+    { 'w',"Write partition table", write_out },
+    { '?',"Help",                  help },
+    {  0,  NULL,                   NULL }
 };
+
+/* Table indexing the valid partition types.
+ *
+ * Any value of 'Y' indicates a valid partition type,
+ *		'.' indicates an invalid partition type.
+ *
+ * The types in this list are those recognised by the fdisk
+ * command that is distributed with Red Hat Linux 6.2.
+ */
+
+char partOK[257] = {   /*	'0123456789ABCDEF'		*/
+
+				"YYYYYYYYYYYYY.YY"	/*  0x	*/
+				"YYY.Y.YYY..YY.Y."	/*  1x	*/
+				"....Y..........."	/*  2x	*/
+				"............Y..."	/*  3x	*/
+				"YYY..........YYY"	/*  4x	*/
+				"YYYYYYY.....Y..."	/*  5x	*/
+				".Y.YYY.........."	/*  6x	*/
+				"Y....Y.........."	/*  7x	*/
+				"YYYYYYYY........"	/*  8x	*/
+				"...YY..........."	/*  9x	*/
+				"Y....YYY........"	/*  Ax	*/
+				".......YY......."	/*  Bx	*/
+				".Y..Y.YY........"	/*  Cx	*/
+				"...........Y...."	/*  Dx	*/
+				".Y.YY......Y...."	/*  Ex	*/
+				".YY.Y........YYY"	/*  Fx	*/
+};
+
+#define valid(n)	(partOK[n])
+
+void list_types()	/* FIXME - Should make this more flexible */
+{
+    printf("0 Empty                   3c PartitionMagic recovery 85 Linux extended\n");
+    printf("1 FAT12                   40 Venix 80286             86 NTFS volume set\n");
+    printf("2 XENIX root              41 PPC PReP Boot           87 NTFS volume set\n");
+    printf("3 XENIX usr               42 SFS                     93 Amoeba\n");
+    printf("4 FAT16 <32M              4d QNX4.x                  94 Amoeba BBT\n");
+    printf("5 Extended                4e QNX4.x 2nd part         a0 IBM Thinkpad hibernate\n");
+    printf("6 FAT16                   4f QNX4.x 3rd part         a5 BSD/386\n");
+    printf("7 HPFS/NTFS               50 OnTrack DM              a6 OpenBSD\n");
+    printf("8 AIX                     51 OnTrack DM6 Aux1        a7 NeXTSTEP\n");
+    printf("9 AIX bootable            52 CP/M                    b7 BSDI fs\n");
+    printf("a OS/2 Boot Manager       53 OnTrack DM6 Aux3        b8 BSDI swap\n");
+    printf("b Win95 FAT32             54 OnTrack DM6             c1 DRDOS/sec FAT-12\n");
+    printf("c Win95 FAT32 (LBA)       55 EZ-Drive                c4 DRDOS/sec FAT-16 <32M\n");
+    printf("e Win95 FAT16 (LBA)       56 Golden Bow              c6 DRDOS/sec FAT-16\n");
+    printf("f Win95 Extended (LBA)    5c Priam Edisk             c7 Syrinx\n");
+    printf("10 OPUS                   61 SpeedStor               db CP/M / CTOS / ...\n");
+    printf("11 Hide FAT12             63 GNU HURD or SysV        e1 DOS access\n");
+    printf("12 Compaq diagnostics     64 Novell Netware 286      e3 DOS R/O\n");
+    printf("14 Hide FAT16 <32M        65 Novell Netware 386      e4 SpeedStor\n");
+    printf("16 Hide FAT16             70 DiskSecure Multi-Boot   eb BeOS fs\n");
+    printf("17 Hide HPFS/NTFS         75 PC/IX                   f1 SpeedStor\n");
+    printf("18 AST Windows swapfile   80 Old Minix               f2 DOS secondary\n");
+    printf("1b Hide Win95 FAT32       81 Minix / old Linux       f4 SpeedStor\n");
+    printf("1c Hide Win95 FAT32 (LBA) 82 Linux swap              fd Linux raid autodetect\n");
+    printf("1e Hide Win95 FAT16 (LBA) 83 Linux                   fe LANstep\n");
+    printf("24 NEC DOS                84 OS/2 Hide C:            ff BBT\n");
+    fflush(stdout);
+}
 
 void quit()
 {
-  exit(1);
+    exit(1);
 }
 
 void list_part()
 {
-  if(*dev!=0)
-    list_partition(NULL);
-}
-
-void list_types()  /* FIXME - Should make this more flexible */
-{
-  printf("\n0 Empty           5 Extended          81 Linux/Minix\n");
-  printf("1 DOS 12-bit FAT  6 DOS 16bit >= 32M  82 Linux swap\n");
-  printf("4 DOS 16bit <32M 80 Old Minix         83 Linux Native\n");
-  fflush(stdout);
+    if(*dev!=0)
+	list_partition(NULL);
 }
 
 void add_part()
 {
-  char buf[8];
-  unsigned char pentry[16];
-  int part,scyl,ecyl;
-  unsigned long tmp;
-  unsigned char *oset;
+    char buf[8];
+    unsigned char pentry[16];
+    int part,scyl,ecyl;
+    unsigned long tmp;
+    unsigned char *oset;
 
-  pentry[0]=0;
+    pentry[0]=0;
 
-  printf("Add partition:\n\n");
-  for(part=0;part<1 || part>4;) {
-    printf("Which partition to add(1-4): ");
-    fflush(stdout);
-    fgets(buf,8,stdin);
-    part=atoi(buf);
-    if(*buf=='\n') return;
-  }
+    printf("Add partition:\n\n");
+    for (part=0;part<1 || part>4;) {
+	printf("Which partition to add(1-4): ");
+	fflush(stdout);
+	fgets(buf,8,stdin);
+	part=atoi(buf);
+	if (*buf=='\n')
+	    return;
+    }
 
-  oset=partitiontable+(0x1be)+((part-1)*16);
+    oset=partitiontable+(0x1be)+((part-1)*16);
 
-  printf("geo.cyl=%d\n",geometry.cylinders);
-  for(scyl=geometry.cylinders+1;scyl<0 || scyl>geometry.cylinders;) {
-    printf("First cylinder(%d-%d):",0,geometry.cylinders);
-    fflush(stdout);
-    fgets(buf,8,stdin);
-    scyl=atoi(buf);
-    if(*buf=='\n') return;
-  }
+    printf("geo.cyl=%d\n",geometry.cylinders);
+    for (scyl=geometry.cylinders+1;scyl<0 || scyl>geometry.cylinders;) {
+	printf("First cylinder(%d-%d):",0,geometry.cylinders);
+	fflush(stdout);
+	fgets(buf,8,stdin);
+	scyl=atoi(buf);
+	if (*buf=='\n')
+	    return;
+    }
 
-  pentry[1]=scyl==0?1:0;
-  pentry[2]=1+((scyl >> 2) & 0xc0);
-  pentry[3]=(scyl&0xff);
+    pentry[1]=scyl==0?1:0;
+    pentry[2]=1+((scyl >> 2) & 0xc0);
+    pentry[3]=(scyl&0xff);
 
 #ifdef PART_TYPE
-  pentry[4]=PART_TYPE;
+    pentry[4]=PART_TYPE;
 #else
-  pentry[4]=0x81;
+    pentry[4]=0x80;
 #endif
 
-  for(ecyl=geometry.cylinders+1;ecyl<scyl || ecyl>geometry.cylinders;) {
-    printf("Ending cylinder(%d-%d):",0,geometry.cylinders);
+    for (ecyl=geometry.cylinders+1;ecyl<scyl || ecyl>geometry.cylinders;) {
+	printf("Ending cylinder(%d-%d):",0,geometry.cylinders);
+	fflush(stdout);
+	fgets(buf,8,stdin);
+	ecyl=atoi(buf);
+	if(*buf=='\n')
+	    return;
+    }
+
+    pentry[5]=geometry.heads-1;
+    pentry[6]=geometry.sectors+((ecyl >> 2) & 0xc0);
+    pentry[7]=(ecyl&0xff);
+
+    tmp=spc*(unsigned long)scyl;
+    if (scyl==0)
+	tmp=(unsigned long)geometry.sectors;
+
+    pentry[11]=(unsigned char)((tmp>>24uL)&0x000000ffuL);
+    pentry[10]=(unsigned char)((tmp>>16uL)&0x000000ffuL);
+    pentry[ 9]=(unsigned char)((tmp>> 8uL)&0x000000ffuL);
+    pentry[ 8]=(unsigned char)((tmp      )&0x000000fful);
+
+    tmp=spc*(unsigned long)(ecyl-scyl+1);
+    if (scyl==0)
+	tmp-=(unsigned long)geometry.sectors;
+
+    pentry[15]=(unsigned char)((tmp>>24uL)&0x000000ffuL);
+    pentry[14]=(unsigned char)((tmp>>16uL)&0x000000ffuL);
+    pentry[13]=(unsigned char)((tmp>> 8uL)&0x000000ffuL);
+    pentry[12]=(unsigned char)((tmp      )&0x000000ffuL);
+
+    printf("Adding partition");
+    memcpy(oset,pentry,16);
+    printf("\n");
     fflush(stdout);
-    fgets(buf,8,stdin);
-    ecyl=atoi(buf);
-    if(*buf=='\n') return;
-  }
-
-  pentry[5]=geometry.heads-1;
-  pentry[6]=geometry.sectors+((ecyl >> 2) & 0xc0);
-  pentry[7]=(ecyl&0xff);
-
-  tmp=spc*(unsigned long)scyl;
-  if(scyl==0) tmp=(unsigned long)geometry.sectors;
-
-  pentry[11]=(unsigned char)((tmp>>24uL)&0x000000ffuL);
-  pentry[10]=(unsigned char)((tmp>>16uL)&0x000000ffuL);
-  pentry[ 9]=(unsigned char)((tmp>> 8uL)&0x000000ffuL);
-  pentry[ 8]=(unsigned char)((tmp      )&0x000000fful);
-
-  tmp=spc*(unsigned long)(ecyl-scyl+1);
-  if(scyl==0) tmp-=(unsigned long)geometry.sectors;
-
-  pentry[15]=(unsigned char)((tmp>>24uL)&0x000000ffuL);
-  pentry[14]=(unsigned char)((tmp>>16uL)&0x000000ffuL);
-  pentry[13]=(unsigned char)((tmp>> 8uL)&0x000000ffuL);
-  pentry[12]=(unsigned char)((tmp      )&0x000000ffuL);
-
-  printf("Adding partition");
-  memcpy(oset,pentry,16);
-  printf("\n");
-  fflush(stdout);
 }
 
 void set_boot()
 {
-  int part,a;
-  char buf[8];
+    int part,a;
+    char buf[8];
 
-  printf("Toggle bootable flag:\n\n");
-  for(part=0;part<1 || part>4;) {
-    printf("Which partition to toggle(1-4): ");
-    fflush(stdout);
-    fgets(buf,8,stdin);
-    part=atoi(buf);
-    if(*buf=='\n') return;
-  }
+    printf("Toggle bootable flag:\n\n");
+    for(part=0;part<1 || part>4;) {
+	printf("Which partition to toggle(1-4): ");
+	fflush(stdout);
+	fgets(buf,8,stdin);
+	part=atoi(buf);
+	if (*buf=='\n')
+	    return;
+    }
 
-  a=(0x1ae)+(part*16);
-  partitiontable[a]=(partitiontable[a]==0x00?0x80:0x00);
+    a=(0x1ae)+(part*16);
+    partitiontable[a]=(partitiontable[a]==0x00?0x80:0x00);
+}
+
+int atoint(s)
+char *s;
+{
+    int n, r;
+
+    n = 0;
+    while (*s) {
+	if (*s >= '0' && *s <= '9')
+	    r = *s - '0';
+	else if (*s >= 'A' && *s <= 'F')
+	    r = *s - 'A' + 10;
+	else if (*s >= 'a' && *s <= 'f')
+	    r = *s - 'a' + 10;
+	else
+	    break;
+	n = 16 * n + r;
+	s++;
+    }
+    return n;
 }
 
 void set_type()  /* FIXME - Should make this more flexible */
 {
-  int part,type,a;
-  char buf[8];
-  printf("Set partition type:\n\n");
-  for(part=0;part<1 || part>4;) {
-    printf("Which partition to toggle(1-4): ");
-    fflush(stdout);
-    fgets(buf,8,stdin);
-    part=atoi(buf);
-    if(*buf=='\n') return;
-  }
-
-  a=(0x1ae)+(part*16)+4;
-  for(type=0;;) {
-    printf("Set partition type to what? (l for list): ");
-    fflush(stdout);
-    fgets(buf,8,stdin);
-    if(*buf=='l')
-      list_types();
-    else {
-      type=atoi(buf);
-      if(type==0 || type==1 || type==4 || type==5 || type==6 ||
-         type==80 || type==81 || type==82 || type==83) break;
-      if(*buf=='\n') return;
+    int part,type,a;
+    char buf[8];
+    printf("Set partition type:\n\n");
+    for (part=0;part<1 || part>4;) {
+	printf("Which partition to toggle(1-4): ");
+	fflush(stdout);
+	fgets(buf,8,stdin);
+	part=atoi(buf);
+	if (*buf=='\n')
+	    return;
     }
-  }
-  if(type>=80) type+=48;
 
-  partitiontable[a]=type;
+    a=(0x1ae)+(part*16)+4;
+    for (type=0;;) {
+	printf("Set partition type to what? (l for list): ");
+	fflush(stdout);
+	fgets(buf,8,stdin);
+	if (*buf=='l')
+	    list_types();
+	else {
+	    type=atoint(buf) % 256;
+	    if (valid(type) == 'Y')
+		break;
+	    if (*buf=='\n')
+		return;
+	}
+    }
+
+    partitiontable[a]=type;
 }
 
 void del_part()
 {
-  int part;
-  char buf[8];
+    int part;
+    char buf[8];
 
   printf("Delete partition:\n\n");
   for(part=0;part<1 || part>4;) {
