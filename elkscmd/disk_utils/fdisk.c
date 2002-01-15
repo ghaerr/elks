998aba5954089f6e92b80766b58c1701a8f51c04
Riley H. Williams <>
2002-01-15 23:02:25 +0000

    Fixed buglet in fdisk.

--- elkscmd/disk_utils/fdisk.c
+++ elkscmd/disk_utils/fdisk.c
@@ -8,7 +8,6 @@
  */
 
 #include <stdio.h>
-#include <ctype.h>
 
 #ifdef linux
 
@@ -35,6 +34,9 @@
 
 #include "fdisk.h"
 
+#define isxdigit(x)	( ((x)>='0' && (x)<='9') || ((x)>='A' && (x)<='F') \
+						 || ((x)>='a' && (x)<='f') )
+
 #define die { printf("Usage %s [-l] <device-name>\n",argv[0]); fflush(stdout); exit(1); }
 #define spc ((unsigned long)(geometry.heads*geometry.sectors))
 
@@ -53,8 +55,8 @@ static Funcs funcs[] = {
 
 /* Table indexing the valid partition types.
  *
- * Any value of 'Y' indicates a valid partition type,
- *		'.' indicates an invalid partition type.
+ * Any value of 'Y' indicates a valid partition type, and
+ * any other value says otherwise.
  *
  * The types in this list are those recognised by the fdisk
  * command that is distributed with Red Hat Linux 6.2.
@@ -80,7 +82,7 @@ char partOK[257] = {   /*	'0123456789ABCDEF'		*/
 				".YY.Y........YYY"	/*  Fx	*/
 };
 
-#define valid(n)	(partOK[n])
+#define valid(n)	(partOK[n] == 'Y')
 
 void list_types()	/* FIXME - Should make this more flexible */
 {
@@ -128,14 +130,13 @@ void list_part()
 
 void add_part()
 {
-    char buf[8];
     unsigned char pentry[16];
-    int part,scyl,ecyl;
+    char buf[8];
     unsigned long tmp;
     unsigned char *oset;
+    int part, scyl, ecyl;
 
     pentry[0]=0;
-
     printf("Add partition:\n\n");
     for (part=0;part<1 || part>4;) {
 	printf("Which partition to add(1-4): ");
@@ -207,8 +208,8 @@ void add_part()
 
 void set_boot()
 {
-    int part,a;
     char buf[8];
+    int part, a;
 
     printf("Toggle bootable flag:\n\n");
     for(part=0;part<1 || part>4;) {
@@ -224,14 +225,14 @@ void set_boot()
     partitiontable[a]=(partitiontable[a]==0x00?0x80:0x00);
 }
 
-int atoint(s)
+int atohex(s)
 char *s;
 {
     int n, r;
 
     n = 0;
     if (!isxdigit(*s))
-	return -1;
+	return 256;
     while (*s) {
 	if (*s >= '0' && *s <= '9')
 	    r = *s - '0';
@@ -249,8 +250,9 @@ char *s;
 
 void set_type()  /* FIXME - Should make this more flexible */
 {
-    int part,type,a;
     char buf[8];
+    int part, type, a;
+
     printf("Set partition type:\n\n");
     for (part=0;part<1 || part>4;) {
 	printf("Which partition to toggle(1-4): ");
@@ -260,17 +262,15 @@ void set_type()  /* FIXME - Should make this more flexible */
 	if (*buf=='\n')
 	    return;
     }
-
     a=(0x1ae)+(part*16)+4;
-    for (type=0;;) {
-	printf("Set partition type to what? (l for list, q to quit): ");
+    type=256;
+    while (!valid(type)) {
+	printf("Set partition type (l for list, q to quit): ");
 	fflush(stdout);
 	fgets(buf,8,stdin);
-	if (isxdigit(*buf)) {
-	    type=atoint(buf) % 256;
-	    if (valid(type) == 'Y')
-		break;
-	} else
+	if (isxdigit(*buf))
+	    type=atohex(buf) % 256;
+	else
 	    switch (*buf) {
 
 		case 'l':
@@ -285,194 +285,185 @@ void set_type()  /* FIXME - Should make this more flexible */
 		    break;
 	    }
     }
-
     partitiontable[a]=type;
 }
 
 void del_part()
 {
-    int part;
     char buf[8];
+    int part;
 
-  printf("Delete partition:\n\n");
-  for(part=0;part<1 || part>4;) {
-    printf("Which partition to delete(1-4): ");
+    printf("Delete partition:\n\n");
+    for (part=0;part<1 || part>4;) {
+	printf("Which partition to delete(1-4): ");
+	fflush(stdout);
+	fgets(buf,8,stdin);
+	part=atoi(buf);
+	if (*buf=='\n')
+	    return;
+    }
+    printf("Deleting partition %d...",part);
+    memset(partitiontable+(0x1be)+((part-1)*16),0,16);
+    printf("Done\n");
     fflush(stdout);
-    fgets(buf,8,stdin);
-    part=atoi(buf);
-    if(*buf=='\n') return;
-  }
-
-  printf("Deleting partition %d..",part);
-  memset(partitiontable+(0x1be)+((part-1)*16),0,16);
-  printf("Done\n");
-  fflush(stdout);
 }
 
 void write_out()
 {
-  int i;
+    int i;
 
-  if(lseek(pFd,0L,SEEK_SET)!=0) {
-    printf("ERROR!  Cannot seek to offset 0.\n");
-  } else {
-    if((i=write(pFd,partitiontable,512))!=512) {
-      printf("ERROR!  Only wrote %d of 512 bytes to the partition table.\n",i);
-      printf("        Table possibly corrupted.\n");
-    } else {
-      printf("Successfully wrote %d bytes to %s\n",i,dev);
-    }
-  }
-  fflush(stdout);
+    if (lseek(pFd,0L,SEEK_SET)!=0)
+	printf("ERROR!  Cannot seek to offset 0.\n");
+    else
+	if ((i=write(pFd,partitiontable,512))!=512) {
+	    printf("ERROR!  Only wrote %d of 512 bytes to the partition table.\n",i);
+	    printf("        Table possibly corrupted.\n");
+	} else
+	    printf("Successfully wrote %d bytes to %s\n",i,dev);
+    fflush(stdout);
 }
 
 void help()
 {
-  Funcs *tmp;
+    Funcs *tmp;
 
-  printf("Key Description\n");
-  for(tmp=funcs;tmp->cmd;tmp++)
-    printf("%c   %s\n",tmp->cmd,tmp->help);
-  fflush(stdout);
+    printf("Key Description\n");
+    for (tmp=funcs;tmp->cmd;tmp++)
+	printf("%c   %s\n",tmp->cmd,tmp->help);
+    fflush(stdout);
 }
 
 void list_partition(devname)
 char *devname;
 {
-  unsigned char ptbl[512],*partition;
-  int i;
-  unsigned long seccnt;
+    unsigned char ptbl[512],*partition;
+    unsigned long seccnt;
+    int i;
 
-  if(devname!=NULL) {
-    if((pFd=open(devname,O_RDONLY))==-1) {
-      printf("Error opening %s (%d)\n",devname,-pFd);
-      exit(1);
+    if (devname!=NULL) {
+	if ((pFd=open(devname,O_RDONLY))==-1) {
+	    printf("Error opening %s (%d)\n",devname,-pFd);
+	    exit(1);
+	}
+	if ((i=read(pFd,ptbl,512))!=512) {
+	    printf("Unable to read first 512 bytes from %s, only read %d bytes\n",
+		   devname,i);
+	    exit(1);
+	}
+    } else
+	memcpy(ptbl,partitiontable,512);
+    printf("                      START                  END\n");
+    printf("Device    Boot  Head Sector Cylinder   Head Sector Cylinder  Type  Sector count\n\n");
+    for (i=0x1be;i<0x1fe;i+=16) {
+	partition=ptbl+i;   /* FIXME /--- gotta be a better way to do this ---\ */
+	seccnt= ((unsigned long) partition[15] << 24uL) + \
+		((unsigned long) partition[14] << 16uL) + \
+		((unsigned long) partition[13] <<  8uL) + \
+		 (unsigned long) partition[12];
+	if (partition[5])
+	    printf("%s%d  %c     %2d    %3d    %5d     %2d    %3d    %5d    %2x    %10ld\n",
+		devname==NULL?dev:devname,1+((i-0x1be)/16),
+		partition[0]==0?' ':(partition[0]==0x80?'*':'?'),
+		partition[1],				     /* Start head */
+		partition[2] & 0x3f,			     /* Start sector */
+		partition[3] | ((partition[2] & 0xc0) << 2), /* Start cylinder */
+		partition[5],				     /* End head */
+		partition[6] & 0x3f,			     /* End sector */
+		partition[7] | ((partition[6] & 0xc0) << 2), /* End cylinder */
+		partition[4],				     /* Partition type */
+		seccnt);				     /* Sector count */
     }
-    if((i=read(pFd,ptbl,512))!=512) {
-      printf("Unable to read first 512 bytes from %s, only read %d bytes\n",
-             devname,i);
-      exit(1);
-    }
-  } else {
-    memcpy(ptbl,partitiontable,512);
-  }
-
-  printf("                      START                  END\n");
-  printf("Device    Boot  Head Sector Cylinder   Head Sector Cylinder  Type  Sector count\n");
-  for(i=0x1be;i<0x1fe;i+=16) {
-    partition=ptbl+i;   /* FIXME /--- gotta be a better way to do this ---\ */
-    seccnt=((unsigned long)partition[15] << 24uL) + \
-	((unsigned long)partition[14] << 16uL) + \
-	((unsigned long)partition[13] << 8uL) + \
-	(unsigned long)partition[12];
-    if(partition[5])
-    printf("%s%d  %c     %2d   %2d   %4d         %2d   %2d     %4d      %2x    %10ld\n",
-           devname==NULL?dev:devname,1+((i-0x1be)/16),
-           partition[0]==0?' ':(partition[0]==0x80?'*':'?'),
-           partition[1],                                /* Start head */
-           partition[2]&0x3f,                           /* Start sector */
-           partition[3] | ((partition[2] & 0xc0) << 2), /* Start cylinder */
-           partition[5],                                /* End head */
-           partition[6]&0x3f,                           /* End sector */
-           partition[7] | ((partition[6] & 0xc0) << 2), /* End cylinder */
-           partition[4],                                /* Partition type */
-           seccnt);                                     /* Sector count */
-  }
-  if(devname!=NULL) close(pFd);
-  fflush(stdout);
+    if (devname!=NULL)
+	close(pFd);
+    fflush(stdout);
 }
 
 int main(argc,argv)
 int argc;
 char *argv[];
 {
-  int i, mode=MODE_EDIT;
+    int i, mode=MODE_EDIT;
 
-  dev[0]=0;
+    dev[0]=0;
+    for (i=1;i<argc;i++) {
+	if (*argv[i]=='/') {
+	    if (*dev!=0) {
+		printf("Can only specify one device on the command line.\n");
+		exit(1);
+	    } else
+		strncpy(dev,argv[i],256); /* FIXME - Should be some value from a header */
+	} else
+	    if (*argv[i]=='-')
+		switch(*(argv[i]+1)) {
+		    case 'l':
+			mode=MODE_LIST;
+			break;
+		    default:
+			printf("Unknown command: %s\n",argv[i]);
+			exit(1);
+		}
+	    else
+		die;
+    }
 
-  for(i=1;i<argc;i++) {
-    if(*argv[i]=='/') {
-      if(*dev!=0) {
-        printf("Can only specify one device on the command line.\n");
-        exit(1);
-      } else {
-        strncpy(dev,argv[i],256); /* FIXME - Should be some value from a header */
-      }
-    } else
-    if(*argv[i]=='-') {
-      switch(*(argv[i]+1)) {
-        case 'l': mode=MODE_LIST;
-                  break;
-        default: printf("Unknown command: %s\n",argv[i]);
-                 exit(1);
-                 break;
-      }
-    } else
-      die
-  }
-
-  if(argc==1)
+    if(argc==1)
 #ifdef DEFAULT_DEV
-    strncpy(dev,DEFAULT_DEV,256);
+	strncpy(dev,DEFAULT_DEV,256);
 #else
-    die;
+	die;
 #endif
 
 
-  if(mode==MODE_LIST) {
-    if(*dev!=0) {
-      list_partition(dev);
-    } else {
-      list_partition("/dev/hda");
-      list_partition("/dev/hdb");
-      /* Other drives */
-    }
-    return(1);
-  }
-
-  if(mode==MODE_EDIT) {
-    Funcs *tmp;
-    int flag=0;
-    char buf[CMDLEN];
-
-    if((pFd=open(dev,O_RDWR))==-1) {
-      printf("Error opening %s (%d)\n",dev,-pFd);
-      exit(1);
+    if (mode==MODE_LIST) {
+	if (*dev!=0)
+	    list_partition(dev);
+	else {
+	    list_partition("/dev/hda");
+	    list_partition("/dev/hdb");
+	    /* Other drives */
+	}
+	return(1);
     }
 
-    if((i=read(pFd,partitiontable,512))!=512) {
-      printf("Unable to read first 512 bytes from %s, only read %d bytes\n",
-             dev,i);
-      exit(1);
+    if (mode==MODE_EDIT) {
+	char buf[CMDLEN];
+	Funcs *tmp;
+	int flag=0;
+
+	if ((pFd=open(dev,O_RDWR))==-1) {
+	    printf("Error opening %s (%d)\n",dev,-pFd);
+	    exit(1);
+	}
+
+	if ((i=read(pFd,partitiontable,512))!=512) {
+	    printf("Unable to read first 512 bytes from %s, only read %d bytes\n",
+		   dev,i);
+	    exit(1);
+	}
+
+	if (ioctl(pFd, HDIO_GETGEO, &geometry)) {
+	    printf("Error getting geometry of disk, exiting.\n");
+	    exit(1);
+	}
+	if (geometry.heads==0 && geometry.cylinders==0 && geometry.sectors==0)
+	    printf("WARNING!  Read disk geometry as 0/0/0.  Things may break.\n");
+	printf("\nGeometry: %d cylinders, %d heads, %d sectors.\n\n",
+		geometry.cylinders,geometry.heads,geometry.sectors);
+	fflush(stdout);
+	while(!feof(stdin)) {
+	    printf("Command%s:",flag==0?" (? for help)":"");
+	    flag=1;
+	    fflush(stdout);
+	    *buf = 0;
+	    if (fgets(buf,CMDLEN-1,stdin)) {
+		printf("\n");
+		for (tmp=funcs; tmp->cmd; tmp++)
+		    if (*buf==tmp->cmd) {
+			tmp->func();
+			break;
+		    }
+	    }
+	}
     }
-
-    if (ioctl(pFd, HDIO_GETGEO, &geometry)) {
-      printf("Error getting geometry of disk, exiting.\n");
-      exit(1);
-    }
-    if(geometry.heads==0 && geometry.cylinders==0 && geometry.sectors==0) {
-      printf("WARNING!  Read disk geometry as 0/0/0.  Things may break.\n");
-    }
-
-    printf("c/h/s=%d/%d/%d\n\n",geometry.cylinders,geometry.heads,geometry.sectors);
-    fflush(stdout);
-
-    while(!feof(stdin)) {
-      printf("Command%s:",flag==0?" (? for help)":""); flag=1;
-      fflush(stdout);
-      *buf = 0;
-      if (fgets(buf,CMDLEN-1,stdin)) {
-        printf("\n");
-
-        for(tmp=funcs;tmp->cmd;tmp++) {
-          if (*buf==tmp->cmd) {
-            tmp->func();
-            break;
-          }
-        }
-      }
-    } /* for */
-  }
-
-  return(0);
+    return(0);
 }
