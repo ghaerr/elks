a06b4c12271b951c48cf6cd1d44fe368b249e8c4
Riley H. Williams <>
2002-04-19 21:26:08 +0000

    ANSI'fied function prototypes in lib subtree.

--- elks/lib/chqueue.c
+++ elks/lib/chqueue.c
@@ -1,20 +1,25 @@
 /* lib/chqueue.c
  * (C) 1997 Chad Page 
  *
- * (Based on the original character queue code by Alan Cox(?)) */
+ * (Based on the original character queue code by Alan Cox(?))
+ */
 
 /* Array queue handling for new tty drivers... rather generic routine, and
  * flexible to boot.  The character queue uses a data structure which 
  * references a seperately set up char array, removing hard limits from the
- * queing code */
+ * queing code
+ */
 
-/* I can't help but think there's a race condition somewhere in here :) */
+/* I can't help but think there's a race condition somewhere in here :)
+ */
 
-/* 'warming' will be done by the tty driver itself when it copies data over */
+/* 'warming' will be done by the tty driver itself when it copies data over
+ */
 
 /* 16 July 2001 : A divide is quite expensive on an 8086 so I changed the 
- * code to use logical addition and not modulo. The only drawback is we have
- * to use only power of two buffer sizes. (Harry Kalogirou) */
+ * code to use logical addition and not modulo. The only drawback is we
+ * have to use only power of two buffer sizes. (Harry Kalogirou)
+ */
 
 #include <linuxmt/config.h>
 #include <linuxmt/wait.h>
@@ -22,114 +27,100 @@
 #include <linuxmt/debug.h>
 #include <linuxmt/types.h>
 
-int chq_init(q, buf, size)
-register struct ch_queue * q;
-unsigned char *buf; 
-int size;
+int chq_init(register struct ch_queue *q, unsigned char *buf, int size)
 {
-	printd_chq3("CHQ: chq_init(%d, %d, %d)\n", q, buf, size);
-	q->buf = buf;
-	q->len = 0;
-	q->tail = 0;
-	q->size = size;
+    printd_chq3("CHQ: chq_init(%d, %d, %d)\n", q, buf, size);
+    q->buf = buf;
+    q->len = q->tail = 0;
+    q->size = size;
 }
 
 #if 0
-int chq_erase(q)
-register struct ch_queue * q;
+
+int chq_erase(register struct ch_queue *q)
 {
-	q->len = q->tail = 0;	
+    q->len = q->tail = 0;
 }
+
 #endif
 
 /* Adds character c, waiting if wait=1 (or otherwise throws out new char) */
-int chq_addch(q, c, wait)
-register struct ch_queue * q;
-unsigned char c;
-int wait;
+int chq_addch(register struct ch_queue *q, unsigned char c, int wait)
 {
-	unsigned int nhead; 
+    unsigned int nhead;
 
-	printd_chq5("CHQ: chq_addch(%d, %c, %d) q->len=%d q->tail=%d\n", q, c, 0, q->len, q->tail);
+    printd_chq5("CHQ: chq_addch(%d, %c, %d) q->len=%d q->tail=%d\n", q, c, 0,
+		q->len, q->tail);
 
-	if (q->len == q->size) {
-		if (wait) {
-			printd_chq("CHQ: addch sleeping\n");
-			interruptible_sleep_on(&q->wq);
-			printd_chq("CHQ: addch waken up\n");
-		}
+    if (q->len == q->size) {
+	if (wait) {
+	    printd_chq("CHQ: addch sleeping\n");
+	    interruptible_sleep_on(&q->wq);
+	    printd_chq("CHQ: addch waken up\n");
 	}
+    }
 
-	if (q->len == q->size) {
-		return -1;
-	}
+    if (q->len == q->size)
+	return -1;
 
-	nhead = (q->tail + q->len) & (q->size - 1);	
-	q->buf[nhead] = c;
-	q->len++; 
-	wake_up(&q->wq);	
-	return 0;
+    nhead = (q->tail + q->len) & (q->size - 1);
+    q->buf[nhead] = c;
+    q->len++;
+    wake_up(&q->wq);
+
+    return 0;
 }
 
 #if 0
-/* Deletes last character in list */ 
-int chq_delch(q)
-register struct ch_queue * q;
+
+/* Deletes last character in list */
+int chq_delch(register struct ch_queue *q)
 {
-	if (q->len == q->size) {
-		q->len--;
-		return 1;
-	}
-	else
-		return 0;
+    if (q->len == q->size) {
+	q->len--;
+	return 1;
+    } else
+	return 0;
 }
+
 #endif
 
 /* Gets tail character, waiting for one if wait != 0 */
-int chq_getch(q, c, wait)
-register struct ch_queue *q;
-register unsigned char *c;
-int wait;
+int chq_getch(register struct ch_queue *q, register unsigned char *c, int wait)
 {
-	int ntail;
-	int retval;
+    int ntail, retval;
 
-	printd_chq6("CHQ: chq_getch(%d, %d, %d) q->len=%d q->tail=%d q->size=%d\n", q, c, wait, q->len, q->tail, q->size);
+    printd_chq6("CHQ: chq_getch(%d, %d, %d) q->len=%d q->tail=%d q->size=%d\n",
+		q, c, wait, q->len, q->tail, q->size);
 
-	if (q->len == 0) {
-		if (wait) {
-			printd_chq("CHQ: getch sleeping\n");
-			interruptible_sleep_on(&q->wq);	
-			printd_chq("CHQ: getch wokeup\n");
-		}
+    if (q->len == 0) {
+	if (wait) {
+	    printd_chq("CHQ: getch sleeping\n");
+	    interruptible_sleep_on(&q->wq);
+	    printd_chq("CHQ: getch wokeup\n");
 	}
-	if (q->len == 0) {
-		return -1;
-	}
+    }
+    if (q->len == 0)
+	return -1;
 
-	retval = q->buf[q->tail];
-	q->tail++;
-	q->tail &= q->size - 1;
-	q->len--;
+    retval = q->buf[q->tail];
+    q->tail++;
+    q->tail &= q->size - 1;
+    q->len--;
 
-	wake_up(&q->wq);
-	if (c != 0) *c = retval;
-	return retval;	
+    wake_up(&q->wq);
+    if (c != 0)
+	*c = retval;
+
+    return retval;
 }
 
-int chq_peekch(q)
-register struct ch_queue *q;
+int chq_peekch(register struct ch_queue *q)
 {
-	if (q->len == 0)
-		return 0;
-
-	return 1;
+    return (q->len != 0);
 }
 
-int chq_full(q)
-register struct ch_queue *q;
+int chq_full(register struct ch_queue *q)
 {
-	if(q->len == q->size)
-		return 1;
-	return 0;
+    return (q->len == q->size)
 }
--- elks/lib/string.c
+++ elks/lib/string.c
@@ -5,373 +5,360 @@
  */
 
 /*
- * stupid library routines.. The optimized versions should generally be found
- * as inline code in <asm-xx/string.h>
+ * stupid library routines.. The optimized versions should generally be
+ * found as inline code in <asm-xx/string.h>
  *
  * These are buggy as well..
  */
- 
+
 #include <linuxmt/types.h>
 #include <linuxmt/string.h>
 
-char * ___strtok = NULL;
+char *___strtok = NULL;
 
 #ifndef __HAVE_ARCH_STRCPY
-char * strcpy(dest,src)
-char * dest;
-char *src;
-{
-	register char *tmp = dest;
 
-	while ((*dest++ = *src++) != '\0')
-		/* nothing */;
-	return tmp;
+char *strcpy(char *dest, char *src)
+{
+    register char *tmp = dest;
+
+    while ((*tmp++ = *src++) != '\0')
+	/* Do nothing */ ;
+
+    return dest;
 }
+
 #endif
 
 #ifndef __HAVE_ARCH_ATOI
-int atoi(number)
-char *number;
-{
-  int n=0,neg=0;
 
-  while(*number <= ' ' && *number)
-    ++number;
-  if (!*number) return 0;
-  if (*number == '-') { neg=1; number++; }
-  else if (*number == '+') number++;
-  while (*number>'/' && *number<':')
-    n=(n*10)+(*number++)-'0';
-  return(neg?-n:n);
+int atoi(char *number)
+{
+    int n = 0, neg = 0;
+
+    while (number && *number <= ' ')
+	++number;
+    if (!*number)
+	return 0;
+    if (*number == '-') {
+	neg = 1;
+	number++;
+    } else if (*number == '+')
+	number++;
+    while (*number > '/' && *number < ':')
+	n = (n * 10) + (*number++) - '0';
+    return (neg ? -n : n);
 }
+
 #endif
 
 #if 0
+
 #ifndef __HAVE_ARCH_STRNCPY
-char * strncpy(dest,src,count)
-char * dest;
-char *src;
-size_t count;
+
+char *strncpy(char *dest, char *src, size_t count)
 {
-	register char *tmp = dest;
+    register char *tmp = dest;
 
-	while (count-- && (*dest++ = *src++) != '\0')
-		/* nothing */;
+    while (count-- && (*tmp++ = *src++))
+	/* Do nothing */ ;
 
-	return tmp;
+    return dest;
 }
+
 #endif
 
 #ifndef __HAVE_ARCH_STRCAT
-char * strcat(dest,src)
-char * dest;
-char * src;
+
+char *strcat(char *dest, char *src)
 {
-	register char *tmp = dest;
+    register char *tmp = dest;
 
-	while (*dest)
-		dest++;
-	while ((*dest++ = *src++) != '\0')
-		;
+    while (*tmp)
+	tmp++;
+    while ((*tmp++ = *src++))
+	/* Do nothing */ ;
 
-	return tmp;
+    return dest;
 }
+
 #endif
 
 #ifndef __HAVE_ARCH_STRNCAT
-char * strncat(dest,src,count)
-char *dest;
-char *src;
-size_t count;
+
+char *strncat(char *dest, char *src, size_t count)
 {
-	register char *tmp = dest;
+    register char *tmp = dest;
 
-	if (count) {
-		while (*dest)
-			dest++;
-		while ((*dest++ = *src++)) {
-			if (--count == 0) {
-				*dest = '\0';
-				break;
-			}
-		}
-	}
+    if (count) {
+	while (*tmp)
+	    tmp++;
+	while ((*tmp++ = *src++) && --count)
+	    /* Do nothing */;
+	*tmp = '\0';
+    }
 
-	return tmp;
+    return dest;
 }
+
 #endif
+
 #endif
 
 #ifndef __HAVE_ARCH_STRCMP
-int strcmp(cs,ct)
-register char * cs;
-register char * ct;
+
+int strcmp(register char *cs, register char *ct)
 {
-	char __res;
-
-	while (1) {
-		if ((__res = *cs - *ct++) != 0 || !*cs++)
-			break;
-	}
-
-	return __res;
+    while (*(cs++) != *(ct++))
+	/* Do nothing */;
+    return *(--cs) - *(--ct);
 }
+
 #endif
 
 #ifndef __HAVE_ARCH_STRNCMP
-int strncmp(cs,ct,count)
-register char * cs;
-register char * ct;
-size_t count;
+
+int strncmp(register char *cs, register char *ct, size_t count)
 {
-	char __res = 0;
-
-	while (count) {
-		if ((__res = *cs - *ct++) != 0 || !*cs++)
-			break;
-		count--;
-	}
-
-	return __res;
+    if (!count)
+	return 0;
+    while (*(cs++) != *(ct++) && --count)
+	/* Do nothing */;
+    return *(--cs) - *(--ct);
 }
+
 #endif
 
 #if 0
+
 #ifndef __HAVE_ARCH_STRCHR
-char * strchr(s,c)
-char * s;
-int c;
+
+char *strchr(char *s, int c)
 {
-	for(; *s != (char) c; ++s)
-		if (*s == '\0')
-			return NULL;
-	return (char *) s;
+    while (*s) {
+	if (*s++ == (char) c)
+	    return s;
+    return NULL;
 }
+
 #endif
+
 #endif
 
 #ifndef __HAVE_ARCH_STRLEN
-size_t strlen(s)
-char * s;
-{
-	register char *sc;
 
-	for (sc = s; *sc != '\0'; ++sc)
-		/* nothing */;
-	return sc - s;
+size_t strlen(char *s)
+{
+    register char *sc;
+
+    for (sc = s; *sc; ++sc)
+	/* Do nothing */ ;
+    return sc - s;
 }
+
 #endif
 
 #ifndef __HAVE_ARCH_STRNLEN
-size_t strnlen(s,count)
-char * s;
-size_t count;
-{
-	register char *sc;
 
-	for (sc = s; count-- && *sc != '\0'; ++sc)
-		/* nothing */;
-	return sc - s;
+size_t strnlen(char *s, size_t count)
+{
+    register char *sc;
+
+    for (sc = s; count-- && *sc; ++sc)
+	/* Do nothing */ ;
+    return sc - s;
 }
+
 #endif
 
 #if 0
+
 #ifndef __HAVE_ARCH_STRSPN
-size_t strspn(s,accept)
-char *s;
-char *accept;
+
+size_t strspn(char *s, char *accept)
 {
-	register char *p;
-	register char *a;
-	size_t count = 0;
+    register char *p, *a;
+    size_t count = 0;
 
-	for (p = s; *p != '\0'; ++p) {
-		for (a = accept; *a != '\0'; ++a) {
-			if (*p == *a)
-				break;
-		}
-		if (*a == '\0')
-			return count;
-		++count;
-	}
+    for (p = s; *p && *a; ++p) {
+	for (a = accept; *a; ++a)
+	    if (*p == *a)
+		break;
+	++count;
+    }
 
-	return count;
+    return count;
 }
+
 #endif
 
 #ifndef __HAVE_ARCH_STRPBRK
-char * strpbrk(cs,ct)
-char * cs;
-char * ct;
-{
-	register char *sc1;
-	register char *sc2;
 
-	for( sc1 = cs; *sc1 != '\0'; ++sc1) {
-		for( sc2 = ct; *sc2 != '\0'; ++sc2) {
-			if (*sc1 == *sc2)
-				return (char *) sc1;
-		}
+char *strpbrk(char *cs, char *ct)
+{
+    register char *sc1, *sc2;
+
+    for (sc1 = cs; *sc1 != '\0'; ++sc1) {
+	for (sc2 = ct; *sc2 != '\0'; ++sc2) {
+	    if (*sc1 == *sc2)
+		return (char *) sc1;
 	}
-	return NULL;
+    }
+    return NULL;
 }
+
 #endif
 
 #ifndef __HAVE_ARCH_STRTOK
-char * strtok(s,ct)
-char * s;
-char * ct;
-{
-	register char *sbegin, *send;
 
-	sbegin  = s ? s : ___strtok;
-	if (!sbegin) {
-		return NULL;
-	}
-	sbegin += strspn(sbegin,ct);
-	if (*sbegin == '\0') {
-		___strtok = NULL;
-		return( NULL );
-	}
-	send = strpbrk( sbegin, ct);
-	if (send && *send != '\0')
-		*send++ = '\0';
-	___strtok = send;
-	return (sbegin);
+char *strtok(char *s, char *ct)
+{
+    register char *sbegin, *send;
+
+    sbegin = s ? s : ___strtok;
+    if (!sbegin) {
+	return NULL;
+    }
+    sbegin += strspn(sbegin, ct);
+    if (*sbegin == '\0') {
+	___strtok = NULL;
+	return (NULL);
+    }
+    send = strpbrk(sbegin, ct);
+    if (send && *send != '\0')
+	*send++ = '\0';
+    ___strtok = send;
+    return (sbegin);
 }
+
 #endif
+
 #endif
 
 #ifndef __HAVE_ARCH_MEMSET
-char * memset(s,c,count)
-char * s;
-char c;
-size_t count;
+
+char *memset(char *s, char c, size_t count)
 {
-	register char *xs = (char *) s;
+    register char *xs = s;
 
-	while (count--)
-		*xs++ = c;
+    while (count--)
+	*xs++ = c;
 
-	return s;
+    return s;
 }
+
 #endif
 
 #if 0
+
 #ifndef __HAVE_ARCH_BCOPY
-char * bcopy(src,dest,count)
-char * src;
-char * dest;
-int count;
+
+char *bcopy(char *src, char *dest, int count)
 {
-	register char *tmp = dest;
+    register char *tmp = dest;
 
-	while (count--)
-		*tmp++ = *src++;
+    while (count--)
+	*tmp++ = *src++;
 
-	return dest;
+    return dest;
 }
+
 #endif
+
 #endif
 
 #ifndef __HAVE_ARCH_MEMCPY
-char * memcpy(dest,src,count)
-char * dest;
-char *src;
-size_t count;
+
+char *memcpy(char *dest, char *src, size_t count)
 {
-	register char *tmp = (char *) dest, *s = (char *) src;
+    register char *tmp = dest, *s = src;
 
-	while (count--)
-		*tmp++ = *s++;
+    while (count--)
+	*tmp++ = *s++;
 
-	return dest;
+    return dest;
 }
+
 #endif
 
 #if 0
+
 #ifndef __HAVE_ARCH_MEMMOVE
-char * memmove(dest,src,count)
-char * dest;
-char *src;
-size_t count;
+
+char *memmove(char *dest, char *src, size_t count)
 {
-	register char *tmp, *s;
+    register char *tmp = dest, *s = src;
 
-	if (dest <= src) {
-		tmp = (char *) dest;
-		s = (char *) src;
-		while (count--)
-			*tmp++ = *s++;
-		}
-	else {
-		tmp = (char *) dest + count;
-		s = (char *) src + count;
-		while (count--)
-			*--tmp = *--s;
-		}
+    if (dest <= src) {
+	while (count--)
+	    *tmp++ = *s++;
+    } else {
+	tmp += count;
+	s += count;
+	while (count--)
+	    *(--tmp) = *(--s);
+    }
 
-	return dest;
+    return dest;
 }
+
 #endif
+
 #endif
 
 #if 0
-#ifndef __HAVE_ARCH_MEMCMP
-int memcmp(cs,ct,count)
-char * cs;
-char * ct;
-size_t count;
-{
-	register unsigned char *su1, *su2;
-	char res = 0;
 
-	for( su1 = cs, su2 = ct; 0 < count; ++su1, ++su2, count--)
-		if ((res = *su1 - *su2) != 0)
-			break;
-	return res;
+#ifndef __HAVE_ARCH_MEMCMP
+
+int memcmp(char *cs, char *ct, size_t count)
+{
+    register unsigned char *su1 = cs, *su2 = ct;
+    char res = 0;
+
+    for (; count > 0; count--)
+	if (!(res = *su1++ - *su2++))
+	    break;
+
+    return res;
 }
+
 #endif
 
 /*
  * find the first occurrence of byte 'c', or 1 past the area if none
  */
 #ifndef __HAVE_ARCH_MEMSCAN
-char * memscan(addr,c,size)
-char * addr;
-int c;
-size_t size;
-{
-	register unsigned char * p = (unsigned char *) addr;
 
-	while (size) {
-		if (*p == c)
-			return (char *) p;
-		p++;
-		size--;
-	}
-  	return (char *) p;
+char *memscan(char *addr, int c, size_t size)
+{
+    register unsigned char *p = (unsigned char *) addr;
+
+    while (size-- && *p != c) {
+	p++;
+    }
+    return (char *) p;
 }
+
 #endif
 
 #ifndef __HAVE_ARCH_STRSTR
-char * strstr(s1,s2)
-char * s1;
-char * s2;
-{
-	int l1, l2;
 
-	l2 = strlen(s2);
-	if (!l2)
-		return (char *) s1;
-	l1 = strlen(s1);
-	while (l1 >= l2) {
-		l1--;
-		if (!memcmp(s1,s2,l2))
-			return (char *) s1;
-		s1++;
-	}
-	return NULL;
+char *strstr(char *s1, char *s2)
+{
+    int l1, l2 = strlen(s2);
+
+    if (!l2)
+	return (char *) s1;
+
+    l1 = strlen(s1);
+    while (l1-- >= l2) {
+	if (!memcmp(s1, s2, l2))
+	    return s1;
+	s1++;
+    }
+    return NULL;
 }
+
 #endif
+
 #endif
