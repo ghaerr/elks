4241d45304f051c49a17ffe6043a2c36d3bb3513
Riley H. Williams <>
2003-07-21 06:03:27 +0000

    Add name of languages, and German template.

--- elksweb2/Functions/data.php
+++ elksweb2/Functions/data.php
@@ -1,19 +1,29 @@
 <?php
 $langitems = array(
+"de" => array(
+"title" => "ELKS: The Embeddable Linux Kernel System", 
+"language" => "Deutsch",
+"meta" => array("A port of Linux for 16-bit Intel architecture."),
+"menu" => array ("Website History", "Download", "FAQ", "History", "Introduction", "Mailing Lists", "News", "Releases", "Home"), 
+"footer" => array ("This document is released under the terms of the <a href=\"http://www.fsf.org/licenses/gpl.html\">GNU General Public Licence</a>, version 2 only and validates as HTML 4.01 Transitional")
+),
 "en" => array(
 "title" => "ELKS: The Embeddable Linux Kernel System", 
+"language" => "English",
 "meta" => array("A port of Linux for 16-bit Intel architecture."),
 "menu" => array ("Website History", "Download", "FAQ", "History", "Introduction", "Mailing Lists", "News", "Releases", "Home"), 
 "footer" => array ("This document is released under the terms of the <a href=\"http://www.fsf.org/licenses/gpl.html\">GNU General Public Licence</a>, version 2 only and validates as HTML 4.01 Transitional")
 ),
 "es" => array(
 "title" => "ELKS: El Sistema Embedible del Linux Kernel",
+"language" => "Español",
 "meta" => array("Un porte de Linux para arquitecturas Intel de 16-bit."),
 "menu" => array ("Historia del Website", "Descargas", "FAQ", "Historia", "Introduccion", "Lista de Correo", "Noticias", "Releases", "Inicio"),
 "footer" => array ("Este documento es liberado bajo los terminos de la <a href=\"http://www.fsf.org/licenses/gpl.html\">Licencia General Publica de GNU</a>, version 2 solamente y valida como HTML 4.01 Transitional")
 ),
 "nl" => array(
 "title" => "ELKS: Het Embeddable Linux Kernel System",
+"language" => "Nederlander",
 "meta" => array ("Een port van Linux voor 16-bit Intel architectuur."),
 "menu" => array ("Website Geschiedenis", "Download", "FAQ", "Geschiedenis", "Introductie", "Mailing Lists", "Nieuws", "Uitgaven", "Start"),
 "footer" => array ("Dit document is alleen uitgegeven onder de voorwaarden van de <a href=\"http://www.fsf.org/licenses/gpl.html\">GNU Algemene Publieke Licentie</a>, versie 2 en gevalideerd als HTML 4.01 Transitional")
