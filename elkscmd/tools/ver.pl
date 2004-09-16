c21fe4e7efcffc3d9ea77d59295b9e1acff65b94
Claudio Matsuoka <>
2004-09-16 20:10:40 +0000

    Parsing EXTRAVERSION, and a little code cleanup.

--- elkscmd/tools/ver.pl
+++ elkscmd/tools/ver.pl
@@ -1,18 +1,11 @@
 #!/usr/bin/perl
 #Get the version from the kernel makefile
-open(MAKEFILE, $ARGV[0]);
 
-while (<MAKEFILE>) {
-        if (/^\s*VERSION\s*=\s*([0-9]{1,4})/) {
-                $ver = $1;
-        }
-        if (/^\s*PATCHLEVEL\s*=\s*([0-9]{1,4})/) {
-                $pat = $1;
-        }
-        if (/^\s*SUBLEVEL\s*=\s*([0-9]{1,4})/) {
-                $sub = $1;
-        }
+while (<>) {
+        /^\s*VERSION\s*=\s*(\d+)/	&& ($ver = $1);
+        /^\s*PATCHLEVEL\s*=\s*(\d+)/	&& ($pat = $1);
+        /^\s*SUBLEVEL\s*=\s*(\d+)/	&& ($sub = $1);
+        /^\s*EXTRAVERSION\s*=\s*(.*)/	&& ($ext = $1);
 }
-print "\nELKS ";
-print $ver, ".", $pat, ".", $sub;
-print "\n\n";
+
+print "\nELKS ${ver}.${pat}.${sub}${ext}\n\n";
