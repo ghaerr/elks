3c302cad12d34de58aba1b166da06c9173a43364
Jody Bruchon <jody@c02ware.com>
2015-03-01 14:46:23 -0500

    Release ELKS 0.2.0 with massive improvements
    Many improvements in this commit:
    
    - Add a script "build.sh" that makes building ELKS dead simple
    
    - Add a script "elkscmd/image_stats.sh" to get detailed image file
      statistics such as free space and inode counts
    
    - Fix failure to build 360K floppy images due to insufficient
      available inodes. mkfs.minix defaults to 128 inodes for a 360K
      Minix filesystem, but the minimal root filesystem "min_rfs"
      would populate the image with well over 128 inodes. A disk image
      that has no free inodes is useless anyway, so the mkfs.minix
      options were modified to make 360 inodes for all images. The
      largest image available still has over 100 inodes available when
      fully populated, so there is plenty of room for the future.
    
    - Add a new disk image type called 'full1722'. This image is made
      to fit on a 1.722MB formatted floppy disk. Most 1.44MB floppies
      can be formatted to 1.722MB using the Linux 'fdformat' command
      and special options. The only reason this has been added is to
      increase the available disk space for capable systems.
    
    - Improved disk image creation options. There are now four ways to
      build all of the disk images and package them up:
      make images - build all image types but don't make an archive
      make images.zip - make images into a Zip archive
      make images.tar.xz - make images into a Gzip-compressed tarball
      make images.tar.xz - make images into a XZ-compressed tarball
      (images.tar.xz compresses to about 1/7 the size of the others)
    
    - Disk image builds will automatically link /bin/sh to either ash
      (preferred) or sash, depending on what is available.
    
    - All disk image build types were tested in QEMU to ensure they
      boot correctly.
    
    - Bump kernel version to 0.2.0 (yay!)
    
    - Pre-built disk images for ELKS 0.2.0 will be made generally
      available at:  https://github.com/jbruchon/elks/releases
    
    Special thanks to Juan Perez-Sanchez for all his recent ELKS
    kernel patches!

---
+++ build.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+
+# Quick and dirty build script for ELKS
+# Probably buggy, but makes from-scratch builds easier
+
+pause () {
+	echo
+	echo -n "Press a key to continue... "
+	read -n 1 -s
+	echo
+}
+
+clean_exit () {
+	E="$1"
+	test -z $1 && E=0
+	if [ $E -eq 0 ]
+		then echo "Build terminated successfully."
+		else echo "Build script has terminated with error $E"
+	fi
+	exit $E
+}
+
+# Disk images cannot be built unless we're UID 0
+if [ "$UID" != "0" ]
+	then echo "WARNING: Disk images can only be built logged in as 'root'"
+fi
+
+# A copy of dev86 is REQUIRED to build!
+if [ ! -e "./dev86" ]
+	then
+	echo "ERROR: You must copy or symlink 'dev86' to the root of the"
+	echo "       ELKS source tree. If you don't have dev86, you can obtain"
+	echo "       a copy at:       https://github.com/lkundrak/dev86"
+	echo "Cannot build without dev86 in the source tree. Aborting."
+	exit 1
+fi
+
+# bcc is required (but has no --version switch, so test for stdio output)
+if [ -z "$(bcc 2>&1)" ]
+	then
+	echo "ERROR: Cannot execute 'bcc'. You must build and install dev86 to"
+	echo "       your system before attempting to build ELKS. Aborting."
+	exit 1
+fi
+
+# Working directory
+WD="$(pwd)"
+
+
+### Clean if asked
+if [ "$1" = "clean" ]
+	then echo
+	echo "Cleaning up. Please wait."
+	sleep 1
+	for X in elks elkscmd elksnet
+		do cd $X; make clean; cd "$WD"
+	done
+	clean_exit 0
+fi
+
+### Kernel build
+echo
+echo "Preparing to build the ELKS kernel. This will invoke 'make menuconfig'"
+echo "for you to configure the system. The defaults should be OK for many"
+echo "systems, but you may want to review them."
+pause
+cd elks || clean_exit 1
+make clean
+make menuconfig || clean_exit 2
+test -e .config || clean_exit 3
+make defconfig || clean_exit 4
+make -j4 || clean_exit 4
+test -e arch/i86/boot/Image || clean_exit 4
+cd "$WD"
+
+
+### dev86 verification
+echo "Verifying dev86 is built."
+sleep 1
+cd dev86 || clean_exit 5
+make || clean_exit 5
+cd "$WD"
+
+
+### elksnet build
+echo "Building 'elksnet'"
+sleep 1
+cd elksnet || clean_exit 6
+make clean
+make || clean_exit 6
+cd "$WD"
+
+
+### elkscmd build
+echo "Building 'elkscmd'"
+sleep 1
+cd elkscmd || clean_exit 7
+make clean
+make || clean_exit 7
+
+
+### Make image files
+test $UID -ne 0 && echo "Skipping image file build (not root)." && clean_exit 0
+make images.zip || clean_exit 8
+make images.tar.gz || clean_exit 8
+make images.tar.xz || clean_exit 8
+cd "$WD"
+
+echo "Images and image file archives are under 'elkscmd'."
+clean_exit 0
---
+++ elkscmd/image_stats.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Show misc. statistics for ELKS disk images
+
+MTPT=/mnt/elks
+IMAGES="root comb comb_net full5 full3 full1722"
+
+echo
+echo "  ELKS disk image file statistics"
+echo "-----------------------------------"
+
+printf "|%7s %s %5s|%5s|%5s|%5s|\n" "Image" '|' "Total" "Used" "Free" "%use"
+echo '+--------+------+-----+-----+-----+'
+umount $MTPT 2>/dev/null
+umount $MTPT 2>/dev/null
+for X in $IMAGES
+	do if [ -e $X ]
+		then
+		mount $X $MTPT
+		INODES="$(find $MTPT | wc -l)"
+		TOTAL="$(df $MTPT | grep "$MTPT" | sed 's/  */ /g' | cut -d' ' -f2)"
+		USED="$(df $MTPT | grep "$MTPT" | sed 's/  */ /g' | cut -d' ' -f3)"
+		FREE="$(df $MTPT | grep "$MTPT" | sed 's/  */ /g' | cut -d' ' -f4)"
+		PCT="$(df $MTPT | grep "$MTPT" | sed 's/  */ /g' | cut -d' ' -f5)"
+		printf "%8s %s %5s %5s %5s %5s\n" "$X" '|' "$TOTAL" "$USED" "$FREE" "$PCT"
+		umount $MTPT
+	fi
+done
+
+echo
