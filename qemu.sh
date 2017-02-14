e024665a47b8452c434ad005f0e11d714bdd56ad
mfld-fr <mfld.fr@gmail.com>
2017-02-14 18:33:48 +0100

    [ne2k] Fix mfld-fr#20 : improve QEMU script

--- qemu.sh
+++ qemu.sh
@@ -2,12 +2,20 @@
 
 # Specify your QEMU system emulator here:
 # qemu=qemu-system-i386
-qemu=qemu-system-x86_64
+# qemu=qemu-system-x86_64
+qemu=qemu-system-i386
 
 # Specify your keyboard mapping here:
-# keyb=us
-keyb=fr
+# keyb=-k en-us
+# keyb=-k fr
+keyb=
+
+# Specify network debugging here:
+# netdebug=-net dump
+netdebug=
 
 # Configure QEMU as pure ISA system
 
-$qemu -nodefaults -name ELKS -monitor stdio -machine isapc -cpu 486 -m 1M -k $keyb -display sdl -vga cirrus -rtc base=utc serial pty -net nic,model=ne2k_isa -net user -fda elkscmd/full3
+$qemu -nodefaults -name ELKS -monitor stdio -machine isapc -cpu 486 -m 1M \
+$keyb -display sdl -vga cirrus -rtc base=utc -serial pty \
+-net nic,model=ne2k_isa -net user $netdebug -fda elkscmd/full3
