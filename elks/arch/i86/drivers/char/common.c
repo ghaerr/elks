f3f1e85912a908965628372fc19812147e3bbfed
mfld-fr <github@mfld.fr>
2018-05-06 16:42:45 +0200

    Remove empty common.c

--- elks/arch/i86/drivers/char/common.c
+++
@@ -1,7 +0,0 @@
-/* This file is currently just a CVS placeholder, but it will eventually
- * hold all of the definitions for the routines that are currently common
- * to many of the source files in this directory, and the associated
- * common.h header file will contain the prototypes for those routines.
- */
-
-#include "common.h"
