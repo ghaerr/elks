df15cd87226cf6596abf8f1db1c46720b6626074
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2019-02-16 16:43:19 +0800

    emu86.sh: allow specifying options to emu86 binary

--- emu86.sh
+++ emu86.sh
@@ -14,7 +14,7 @@ ${MAKE-make} -C $EMU86_DIR emu86 pcat
 # Root filesystem @ segment 0x8000 (assumes 512K RAM & 512K ROM)
 # Skip the INT 19h bootstrap in the kernel image (+0x42)
 
-$EMU86_DIR/emu86 -w 0xe0000 -f elks/arch/i86/boot/Image -w 0x80000 -f image/romfs.bin -x 0xe000:0x42 &
+$EMU86_DIR/emu86 -w 0xe0000 -f elks/arch/i86/boot/Image -w 0x80000 -f image/romfs.bin -x 0xe000:0x42 ${1+"$@"} &
 
 # Give time to EMU86 to emulate the serial port
 # and store the used PTY in 'emu86.pts'
