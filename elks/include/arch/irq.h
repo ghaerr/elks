4c09804f252072c325807af8a2cfe051775d35eb
mfld-fr <github@mfld.fr>
2019-03-16 17:13:31 +0100

    [kernel] Remove unused halve stuff

--- elks/include/arch/irq.h
+++ elks/include/arch/irq.h
@@ -5,57 +5,18 @@
 
 #include <arch/asm.h>
 
-/*	FIXME:	Who gets which entry in bh_base. Things which will occur
- *		most often should come first - in which case NET should
- *		be up the top with SERIAL/TQUEUE!
- */
-
-enum {
-    TIMER_BH = 0,
-    CONSOLE_BH,
-    TQUEUE_BH,
-    DIGI_BH,
-    SERIAL_BH,
-    RISCOM8_BH,
-    SPECIALIX_BH,
-    BAYCOM_BH,
-    NET_BH,
-    IMMEDIATE_BH,
-    KEYBOARD_BH,
-    CYCLADES_BH,
-    CM206_BH
-};
-
 extern void disable_irq(unsigned int);
 extern void enable_irq(unsigned int);
 extern void do_IRQ(int,void *);
 extern int request_irq(int,void (*)(int,struct pt_regs *,void *),void *);
 extern void free_irq(unsigned int);
 extern void init_IRQ(void);
-extern void do_bottom_half(void);
 
 #ifdef CONFIG_IDLE_HALT
-extern void idle_halt ();
+void idle_halt (void);
 #endif
-
-extern int bh_mask_count[16];
-
-extern unsigned bh_active;
-extern unsigned bh_mask;
-
-extern void (*bh_base[16]) (void);
-
-/*@-namechecks@*/
 
 #ifdef __KERNEL__
-
-#ifdef __BCC__
-#define save_flags(x)	x=__save_flags()
-extern flag_t __save_flags(void);
-extern void restore_flags(flag_t);
-#define clr_irq()	asm("cli")
-#define set_irq()	asm("sti")
-#endif
 
 #ifdef __ia16__
 #define save_flags(x) \
@@ -68,46 +29,30 @@ __asm__ __volatile__("pushw %0\n" \
           "        popfw\n" \
           : /* no output */ :"r" (x):"memory")
 
-#define clr_irq()	asm("cli")
-#define set_irq()	asm("sti")
-#endif
+// From Linux arch/x86/include/asm/irqflags.h
 
-#ifdef __WATCOMC__
-#define clr_irq()	_asm {cli}
-#define set_irq()	_asm {sti}
-#endif
+static inline void irq_disable(void)
+{
+	asm volatile("cli": : :"memory");
+}
 
-#else
+static inline void irq_enable(void)
+{
+	asm volatile("sti": : :"memory");
+}
+
+#define clr_irq irq_disable
+#define set_irq irq_enable
+
+#endif  // __ia16__
+
+#else  // __KERNEL__
 
 #define save_flags(x)
 #define restore_flags(x)
 #define clr_irq()
 #define set_irq()
 
-#endif
-
-#ifdef ENDIS_BH
-
-#define init_bh(nr, routine) do { \
-		bh_base[nr] = routine; \
-		bh_mask_count[nr] = 0; \
-		bh_mask |= 1 << nr; \
-	} until 0
-
-#define disable_bh(nr) do { \
-		bh_mask &= ~(1 << nr); \
-		bh_mask_count[nr]++; \
-	} until 0
-
-#define enable_bh(nr) if (!--bh_mask_count[nr]) \
-	bh_mask |= 1 << nr
-
-#else
-
-#define init_bh(nr, routine) bh_base[nr] = routine
-
-#endif
-
-#define mark_bh(nr) set_bit(nr, &bh_active)
+#endif  // __KERNEL__
 
 #endif
