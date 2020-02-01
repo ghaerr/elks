b83f414def4a5d5c639cf621c4213aa0874eaaba
mfld-fr <github@mfld.fr>
2020-02-01 16:04:18 +0100

    [boot] More generic naming

--- elkscmd/bootblocks/boot_err.h
+++ elkscmd/bootblocks/boot_err.h
@@ -1,6 +1,7 @@
 
 #define ERR_SUCCESS     0
-#define ERR_SECTOR_2    1
-#define ERR_DISK_READ   2
-#define ERR_NOT_ELKS    3
-#define ERR_NO_SYSTEM   4
+#define ERR_DISK_READ   1
+#define ERR_PAYLOAD     2
+#define ERR_NO_SYSTEM   3
+#define ERR_BAD_SYSTEM  4
+
