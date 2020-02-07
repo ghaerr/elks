829dc4653e329834b33378b3e79c030f7f5254ed
Marcin Laszewski <marcin.laszewskiat>
2020-02-07 19:53:17 +0100

    [busyelks] First working commands

--- busyelks/main.c
+++ busyelks/main.c
@@ -1,30 +1,28 @@
-#define BUSYELKS1
-
-#include <stdio.h>
-#include "config.h"
-#include "applets.h"
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
-	char *slash = strrchr(argv[0], '/');
-	char progname[32];
-	int q;
+	char * progname = strrchr(argv[0], '/');
 
-	if (!slash || (slash == argv[0] && !slash[1]))
-		strcpy(progname, argv[0]);
-	else strcpy(progname, (slash + 1));
+	if(!progname || (progname == argv[0] && !progname[1]))
+		progname = argv[0];
+	else
+		progname++;
 
 	/* If our name was called, assume argv[1] is the command */
-	if(strcmp(progname, "busyelks1") == 0) {
-		strcpy(progname, argv[1]);
+	if(strcmp(progname, "busyelks") == 0) {
+		progname = argv[1];
 	}
-
-#include "applets.c"
 
 	if(strcmp(progname, "true") == 0) exit(0);
 	if(strcmp(progname, "false") == 0) exit(1);
+	/*
 	if(strcmp(progname, "sync") == 0) { sync(); exit(0); }
+	*/
 
-
-usage();
+	/*
+	usage();
+	*/
+	return 1;
 }
