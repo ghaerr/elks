03b6da58253bdecfd841b2abe514ea66780e7b4a
Marcin Laszewski <marcin.laszewski@gmail.com>
2020-02-10 21:01:15 +0100

    [busyelks] Fix date bug

--- busyelks/cmd/date.c
+++ busyelks/cmd/date.c
@@ -37,7 +37,7 @@ struct timeval *tv;
 	char * p;
 	struct tm tm;
 	
-	p = strtok(datestring, '-');
+	p = strtok(datestring, "-");
 	
 	tm.tm_year= tm.tm_mon= tm.tm_mday= tm.tm_hour= tm.tm_min= tm.tm_sec=0;
 
