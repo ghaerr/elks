7bdcda3e7f2eabe1c92450199951921293eeec6a
tkchia <tkchia@users.noreply.github.com>
2020-03-07 02:35:25 +0800

    [boot] [elks] allow bootloaders to load /linux as one blob (#422)
    Previously, bootloaders have to do the job of splitting up
    /linux into its "setup" portion (which goes to INITSEG:0 and
    SETUPSEG:0) and its "kernel" portion (SYSSEG:0) in memory.
    
    With this change, a bootloader can load /linux as one single
    binary blob at INITSEG:0, and inform /linux's "setup" portion
    about this (via a new elks_flags field in the loaded /linux).
    
    This helps simplify the logic (and shrink the sizes) of the
    Minix and FAT filesystems' boot code.

--- elks/include/linuxmt/boot.h
+++ elks/include/linuxmt/boot.h
@@ -16,4 +16,14 @@
 #define ROOTFLAGS	RF_NONE
 #endif
 
+/* ELKS flags */
+
+#define EF_NONE		0
+#define EF_AS_BLOB	0x8000		/* says that setup and kernel are
+					   loaded as one blob at SETUPSEG:0,
+					   and setup may need to move kernel
+					   to SYSSEG:0 */
+
+#define ELKSFLAGS	0
+
 #endif
