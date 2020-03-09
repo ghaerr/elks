29a1be75743251c6f73e02d91ac5d4db0fa5070b
Jody Bruchon <jody@jodybruchon.com>
2020-03-08 21:57:09 -0400

    busyelks/cmd/uname.c: code style and mixed whitespace cleanups

--- elkscmd/busyelks/cmd/uname.c
+++ elkscmd/busyelks/cmd/uname.c
@@ -2,8 +2,6 @@
 #include <string.h>
 #include <sys/utsname.h>
 
-static void print_element ();
-
 /* Values that are bitwise or'd into toprint'. */
 /* Operating system name. */
 #define PRINT_SYSNAME 1
@@ -23,53 +21,57 @@ static void print_element ();
 /* Mask indicating which elements of the name to print. */
 static unsigned char toprint;
 
-void
-uname_main (argc, argv)
-	int argc;
-	char **argv;
+
+static void print_element(unsigned char mask, char *element)
+{
+	if (toprint & mask) {
+		toprint &= ~mask;
+		write(STDOUT_FILENO,element,strlen(element));
+		write(STDOUT_FILENO,toprint ? " " : "\n",1);
+	}
+}
+
+
+void uname_main (int argc, char **argv)
 {
 	int	i;
 	struct utsname name;
 
 	toprint = 0;
 
-	for(i=1;i<argc;i++)
-	{
-		switch(argv[i][1])
-		{
-	        case 's':
-        		toprint |= PRINT_SYSNAME;
-	        	break;
+	for (i = 1; i < argc; i++) {
+		switch (argv[i][1]) {
+		case 's':
+			toprint |= PRINT_SYSNAME;
+			break;
 
 		case 'n':
-          		toprint |= PRINT_NODENAME;
-          		break;
+			toprint |= PRINT_NODENAME;
+			break;
 
-	        case 'r':
-        		toprint |= PRINT_RELEASE;
-          		break;
+		case 'r':
+			toprint |= PRINT_RELEASE;
+			break;
 
-        	case 'v':
-          		toprint |= PRINT_VERSION;
-          		break;
+		case 'v':
+			toprint |= PRINT_VERSION;
+			break;
 
-        	case 'm':
-          		toprint |= PRINT_MACHINE;
-          		break;
+		case 'm':
+			toprint |= PRINT_MACHINE;
+			break;
 
-        	case 'a':
-          		toprint = PRINT_SYSNAME | PRINT_NODENAME |
-            		PRINT_RELEASE | PRINT_VERSION | PRINT_MACHINE;
-          		break;
+		case 'a':
+			toprint = PRINT_SYSNAME | PRINT_NODENAME |
+			PRINT_RELEASE | PRINT_VERSION | PRINT_MACHINE;
+			break;
 
 		default:
 			break;
 		}
 	}
-	if (toprint == 0)
-		toprint = PRINT_SYSNAME;
-	if (uname (&name) != -1)
-	{
+	if (toprint == 0) toprint = PRINT_SYSNAME;
+	if (uname (&name) != -1) {
 		print_element (PRINT_SYSNAME, name.sysname);
 		print_element (PRINT_NODENAME, name.nodename);
 		print_element (PRINT_RELEASE, name.release);
@@ -79,17 +81,3 @@ uname_main (argc, argv)
 
 	exit(0);
 }
-
-static void
-print_element (mask, element)
-	unsigned char mask;
-	char *element;
-{
-	if (toprint & mask)
-	{
-		toprint &= ~mask;
-		write(STDOUT_FILENO,element,strlen(element));
-		write(STDOUT_FILENO,toprint ? " " : "\n",1);
-	}
-}
-
