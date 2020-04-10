d052b846c44e35fd8d1f28ad1537847e50273432
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2020-04-10 16:07:26 +0800

    [libc] implement snprintf and vsnprintf

---
+++ libc/stdio/vsnprintf.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int
+vsnprintf(char *sp, size_t size, __const char *fmt, va_list ap)
+{
+	static FILE string[1] =
+	{
+		{
+			0,
+			0,
+			(unsigned char *)(unsigned)-1,
+			0,
+			(unsigned char *)(unsigned)-1,
+			-1,
+		 _IOFBF | __MODE_WRITE
+		 }
+	};
+
+	int rv;
+
+	char bogo[1];
+
+	if (size == 0)
+	{
+		sp = bogo;
+		size = 1;
+	}
+
+	string->bufpos = (unsigned char *)sp;
+	string->bufwrite = string->bufend = (unsigned char *)(sp + size - 1);
+	rv = vfprintf(string, fmt, ap);
+	*(string->bufpos) = 0;
+
+	return rv;
+}
