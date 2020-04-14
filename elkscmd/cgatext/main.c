018d965c9f95ea3688dd160b141328097dcf94c4
Marcin Laszewski <Marcin.Laszewski@gmail.com>
2020-04-14 07:46:37 +0200

    Add /dev/cgatext demo.

--- elkscmd/cgatext/main.c
+++ elkscmd/cgatext/main.c
@@ -2,6 +2,7 @@
 2020.04.13	Marcin.Laszewski@gmail.com	CGATEXT Demo
 */
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,6 +23,20 @@ void cgatext_puts(unsigned addr, unsigned short attr, char const *str)
 	cgatext_puts(cgatext_offset((cgatext_COLS - strlen(str)) >> 1, cgatext_ROWS >> 1), attr, str)
 
 #define	rand_c(first_c, last_c)	((first_c) + (rand() % ((last_c) - (first_c) + 1)))
+
+static unsigned
+buf_puts(unsigned short * buf, unsigned char fg, unsigned char bg, char const * text)
+{
+	unsigned n = 0;
+
+	while(*text)
+	{
+		*buf++ = cgatext_cell(fg, bg, *text++);
+		n += cgatext_cell_SIZE;
+	}
+
+	return n;
+}
 
 int main(void)
 {
@@ -95,4 +110,98 @@ int main(void)
 				cgatext_puts(cgatext_offset(10 * x, y), cgatext_cell_color(y, x), text);
 			}
 	}
+
+	sleep(1);
+
+	{
+		char const * devname = "/dev/cgatext";
+
+		int f = open(devname, O_RDWR);
+
+		if(f < 0)
+		{
+			perror(devname);
+			return 1;
+		}
+
+		{
+			unsigned short buf[50];
+			off_t i;
+
+			/* Clear screen */
+			*buf = cgatext_cell(cgatext_color_YELLOW | cgatext_attr_BOLD, cgatext_color_BLUE, ' ');
+			while(write(f, buf, sizeof *buf) > 0);
+
+			{
+				unsigned n;
+
+				n = buf_puts(buf,
+							cgatext_color_CYAN | cgatext_attr_BOLD,
+							cgatext_color_BLUE,
+							"--- /dev/cgatext ---");
+				lseek(f, cgatext_offset((cgatext_COLS - 19) >> 1, cgatext_ROWS >> 1), SEEK_SET);
+				write(f, buf, n);
+			}
+
+			sleep(3);
+
+			{
+				unsigned short c;
+
+				for(i = 0; i < 2000000; i++)
+				{
+					c = cgatext_cell(
+								rand() % cgatext_attr_MAX,
+								rand() % cgatext_color_MAX,
+								'0' + (rand() % 10));
+					lseek(f,
+						cgatext_offset(rand() % cgatext_COLS, rand() % cgatext_ROWS),
+						SEEK_SET);
+					write(f, &c, sizeof c);
+				}
+			}
+
+			lseek(f, 0, SEEK_SET);
+			memset(buf, 0, sizeof(buf));
+			while(write(f, buf, sizeof buf) > 0);
+
+			lseek(f, 0, SEEK_SET);
+
+			{
+				unsigned char fg = cgatext_color_BLACK;
+
+				for(;;)
+				{
+					unsigned n = buf_puts(buf, fg, cgatext_color_BLACK, "ELKS");
+
+					if(write(f, buf, n) != n)
+						break;
+
+					if(++fg >= cgatext_attr_MAX)
+						fg = cgatext_color_BLACK;
+
+					lseek(f, cgatext_cell_SIZE, SEEK_CUR);
+				}
+			}
+
+			{
+				unsigned n;
+
+				n = buf_puts(buf,
+							cgatext_color_GREEN | cgatext_attr_BOLD,
+							cgatext_color_BLACK,
+							" -THE END- ");
+				lseek(f, n, SEEK_END);
+				write(f, buf, n);
+			}
+		}
+
+		if(close(f) < 0)
+		{
+			perror("close(cgatext)");
+			return 2;
+		}
+	}
+
+	return 0;
 }
