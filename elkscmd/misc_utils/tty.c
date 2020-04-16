4dffc6ad8b22e62f8a1fabfa2c7a07182e66ccea
Helge Skrivervik <helge@iMac27.local>
2020-04-16 11:44:13 +0200

    Added tty(1) command to misc_utils.

---
+++ elkscmd/misc_utils/tty.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * Type tty name
+ */
+
+extern char    *ttyname();
+
+int main(int argc, char **argv)
+{
+        register char *p;
+
+        p = ttyname(0);
+        if(argc==2 && !strcmp(argv[1], "-s"))
+                ;	/* silent mode,  just exit code */
+        else
+                printf("%s\n", (p? p: "not a tty"));
+        exit(p? 0: 1);
+}
+
