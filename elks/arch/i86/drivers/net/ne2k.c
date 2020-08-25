80ab3c78945331af53b0dd5d7dafe1c71eac6487
Helge Skrivervik <helge@mymayday.com>
2020-08-25 14:06:43 +0200

    Typo!!

--- elks/arch/i86/drivers/net/ne2k.c
+++ elks/arch/i86/drivers/net/ne2k.c
@@ -20,7 +20,7 @@
 
 #include "ne2k.h"
 
-#define DEBUG_ETH	0 /* set =1 for debugging*/
+#define DEBUG_ETH	1 /* set =1 for debugging*/
 
 #if DEBUG_ETH
 #define debug_eth   printk
@@ -190,10 +190,12 @@ static void ne2k_int(int irq, struct pt_regs * regs, void * dev_id)
 	}
 
 	if (stat & NE2K_STAT_RX) {
+		printk("|r|");
 		wake_up(&rxwait);
 	}
 
 	if (stat & NE2K_STAT_TX) {
+		printk("|t|");
 		wake_up(&txwait);
 	}
 	if (stat & NE2K_STAT_RDC) {
