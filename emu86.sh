1d146ed2c8f30f66dcafae1f9b344515b047b101
Greg Haerr <greg@censoft.com>
2020-10-16 23:41:16 -0700

    [kernel] Cleanup setup.S for CONFIG_ROMCODE, REL_SYS and !REL_SYS

--- emu86.sh
+++ emu86.sh
@@ -9,9 +9,9 @@
 # First run ELKS with kernel and root FS in ROM
 # Kernel image @ segment 0xE000 (top of 1024K address space)
 # Root filesystem @ segment 0x8000 (assumes 512K RAM & 512K ROM)
-# Skip the INT 19h bootstrap in the kernel image (+0x32)
+# Skip the INT 19h bootstrap in the kernel image (+0x14)
 
-exec ../emu86/emu86 -w 0xe0000 -f elks/arch/i86/boot/Image -w 0x80000 -f image/romfs.bin -x 0xe000:0x32 ${1+"$@"}
+exec ../emu86/emu86 -w 0xe0000 -f elks/arch/i86/boot/Image -w 0x80000 -f image/romfs.bin -x 0xe000:0x14 ${1+"$@"}
 
 # Give time to EMU86 to emulate the serial port
 # and store the used PTY in 'emu86.pts'
