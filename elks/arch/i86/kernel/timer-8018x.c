1661a3cbd8f3c3ebd367631391c2793e07534808
Santiago Hormazabal <santiagohssl@gmail.com>
2021-05-13 00:32:15 -0300

    8018x: add integrated PTU timer

---
+++ elks/arch/i86/kernel/timer-8018x.c
@@ -0,0 +1,63 @@
+/*
+ * 8018X's Integrated Timer/Counter Unit
+ *
+ * This file contains code used for the embedded 8018X family only.
+ * 
+ * 16 May 21 Santiago Hormazabal
+ */
+
+#include <linuxmt/config.h>
+#include <linuxmt/config.h>
+#include <linuxmt/mm.h>
+#include <linuxmt/timer.h>
+
+#include <arch/io.h>
+#include <arch/irq.h>
+#include <arch/param.h>
+#include <arch/ports.h>
+
+/*
+ * Main goal is to have Timer2 generating a 1ms period signal, so
+ * calculate the appropriate value of the prescaler counter.
+ * Note that 1kHz corresponds to 1ms period. Timer2 is clocked
+ * by 1/4 of FCPU.
+ *
+ * Timer2Interval = (FCPU / 4) / 1kHz
+ */
+#define TIMER2_INTERVAL (CONFIG_8018X_FCPU * 1000000L / (4 * 1000))
+
+/*
+ * With a reference of 1ms (1kHz) coming into Timer0, calculate how
+ * many counts are needed to achieve the "HZ" rate (usually 100Hz).
+ *
+ * Timer0Interval = 1kHz / HZ
+ */
+#define TIMER0_INTERVAL (1000 / HZ)
+
+void enable_timer_tick(void)
+{
+    /* Set Timer2 Maxcount register */
+    outw(TIMER2_INTERVAL, 0xff00 + 0x42); /* T2CMPA */
+    /* Clear the Timer count register before starting it */
+    outw(0x0000, 0xff00 + 0x40); /* T2CNT */
+
+    /* Set Timer0 Maxcount register */
+    outw(TIMER0_INTERVAL, 0xff00 + 0x32); /* T0CMPA */
+    /* Clear the Timer count register before starting it */
+    outw(0x0000, 0xff00 + 0x30); /* T0CNT */
+
+    /* Enable Timer2, release inhibit to change EN bit, continuous mode */
+    outw(0xc001, 0xff00 + 0x46); /* T2CON */
+
+    /* Enable Timer0, release inhibit to change EN bit, INT enabled, use Timer2 as source, continuous mode */
+    outw(0xe009, 0xff00 + 0x36); /* T0CON */
+}
+
+void disable_timer_tick(void)
+{
+    /* Disable Timer2, release inhibit to change EN bit */
+    outw(0x4000, 0xff00 + 0x46); /* T2CON */
+
+    /* Disable Timer0, release inhibit to change EN bit */
+    outw(0x4000, 0xff00 + 0x36); /* T0CON */
+}
