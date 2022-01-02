c31a2b6b31e6daf6b46d418d40ce4510fc2ea667
tyama501 <61556504+tyama501@users.noreply.github.com>
2022-01-03 04:03:14 +0900

    Add kbd-poll-pc98 and modified conio-pc98 for Arrow keys and Page Up, Page Down

---
+++ elks/arch/i86/drivers/char/kbd-poll-pc98.c
@@ -0,0 +1,84 @@
+/*
+ * Polling keyboard driver
+ *
+ * Calls conio_poll to get kbd input
+ */
+#include <linuxmt/types.h>
+#include <linuxmt/config.h>
+#include <linuxmt/timer.h>
+#include <linuxmt/sched.h>
+#include "console.h"
+#include "conio.h"
+
+char kbd_name[] = "polling";
+
+static void restart_timer(void);
+
+/*
+ * Poll for keyboard character
+ * Decodes non-zero high byte into arrow/fn keys
+ */
+static void kbd_timer(int data)
+{
+    int dav, extra = 0;
+
+    if ((dav = conio_poll())) {
+	if (dav & 0xFF)
+	    Console_conin(dav & 0x7F);
+	else {
+	    dav = (dav >> 8) & 0xFF;
+
+	    if (dav & 0x4)
+	        dav = 'A';	/* up*/
+	    else if (dav & 0x20)
+	        dav = 'B';	/* down*/
+	    else if (dav & 0x10)
+	        dav = 'C';	/* right*/
+	    else if (dav & 0x8)
+	        dav = 'D';	/* left*/
+	    else if (dav & 0x80) {		/* Roll Down for PC-98 */
+	        dav = '5';	/* pgup*/
+	        extra = '~';
+	    }
+	    else if (dav & 0x40) {		/* Roll Up for PC-98 */
+	        dav = '6';	/* pgdn*/
+	        extra = '~';
+	    }
+	    else
+	        dav = 0;
+
+	    if (dav) {
+		Console_conin(033);
+#ifdef CONFIG_EMUL_ANSI
+		Console_conin('[');
+#endif
+		Console_conin(dav);
+#ifdef CONFIG_EMUL_ANSI
+		if (extra)
+		    Console_conin(extra);
+#endif
+	    }
+	}
+    }
+    restart_timer();
+}
+
+static void restart_timer(void)
+{
+    static struct timer_list timer;
+
+    init_timer(&timer);
+    timer.tl_expires = jiffies + (8 * HZ/100);	/* every 8/100 second*/
+    timer.tl_function = kbd_timer;
+    add_timer(&timer);
+}
+
+
+void kbd_init(void)
+{
+    conio_init();
+#if 0
+    enable_irq(1);		/* enable BIOS Keyboard interrupts */
+#endif
+    restart_timer();
+}
