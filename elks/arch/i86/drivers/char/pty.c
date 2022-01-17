c5686fbe4f043c5f356960852d9c8ea67d9794cb
Greg Haerr <greg@censoft.com>
2022-01-16 19:07:39 -0700

    [kernel] Fix pty buffer overread bug on signals

--- elks/arch/i86/drivers/char/pty.c
+++ elks/arch/i86/drivers/char/pty.c
@@ -122,10 +122,9 @@ size_t pty_write (struct inode *inode, struct file *file, char *data, size_t len
 		}
 
 		ret = get_user_char ((void *)(data++));
-		if (!tty_intcheck(tty, ret)) {
+		if (!tty_intcheck(tty, ret))
 			chq_addch_nowakeup (&tty->inq, ret);
-			count++;
-		}
+		count++;
 	}
 	if (count > 0)
 		wake_up(&tty->inq.wait);
