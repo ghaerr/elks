4c25fdf583bdba943932708d31a81c9243c4f49c
tyama501 <61556504+tyama501@users.noreply.github.com>
2022-08-07 03:44:00 +0900

    [nano-X] Add init code for PC-98 screen

--- elkscmd/nano-X/drivers/scr_pc98.c
+++ elkscmd/nano-X/drivers/scr_pc98.c
@@ -51,6 +51,13 @@
 
 #define static
 
+#define LIOSEG 0xF990 /* ROM Segment for LIO */
+#define LIOINT 0xA0   /* Starting LIO interrupt number */
+
+#define LIO_M_SIZE 5200
+
+#define _MK_FP(seg,off) ((void __far *)((((unsigned long)(seg)) << 16) | (off)))
+
 /* VGA driver entry points*/
 static int  VGA_open(PSD psd);
 static void VGA_close(PSD psd);
@@ -82,14 +89,96 @@ SCREENDEVICE	scrdev = {
 /* operating mode*/
 static BOOL VGAMODE = TRUE;	/* ega or vga screen rows*/
 
-/* int10 functions*/
-#define FNGR640x480	0x0012	/* function for graphics mode 640x480x16*/
-#define FNGR640x350	0x0010	/* function for graphics mode 640x350x16*/
-#define FNTEXT		0x0003	/* function for 80x25 text mode*/
+void int_A0(unsigned int l_seg)
+{
+    __asm__ volatile ("push %ds;"
+                      "push %es;"
+                      "push %bp;"
+                      "push %si;"
+                      "push %di;");
+    __asm__ volatile ("mov %0,%%ds;"
+                      "int $0xA0;"
+                      :
+                      :"a" (l_seg)
+                      :"memory", "cc");
+    __asm__ volatile ("pop %di;"
+                      "pop %si;"
+                      "pop %bp;"
+                      "pop %es;"
+                      "pop %ds;");
+}
+
+void int_A1(unsigned int l_seg)
+{
+    __asm__ volatile ("push %ds;"
+                      "push %es;"
+                      "push %bp;"
+                      "push %si;"
+                      "push %di;");
+    __asm__ volatile ("mov %0,%%ds;"
+                      "mov $0x0000,%%bx;"
+                      "int $0xA1;"
+                      :
+                      :"a" (l_seg)
+                      :"memory", "cc");
+    __asm__ volatile ("pop %di;"
+                      "pop %si;"
+                      "pop %bp;"
+                      "pop %es;"
+                      "pop %ds;");
+}
+
+void int_A2(unsigned int l_seg)
+{
+    __asm__ volatile ("push %ds;"
+                      "push %es;"
+                      "push %bp;"
+                      "push %si;"
+                      "push %di;");
+    __asm__ volatile ("mov %0,%%ds;"
+                      "mov $0x0000,%%bx;"
+                      "int $0xA2;"
+                      :
+                      :"a" (l_seg)
+                      :"memory", "cc");
+    __asm__ volatile ("pop %di;"
+                      "pop %si;"
+                      "pop %bp;"
+                      "pop %es;"
+                      "pop %ds;");
+}
+
+void int_A3(unsigned int l_seg)
+{
+    __asm__ volatile ("push %ds;"
+                      "push %es;"
+                      "push %bp;"
+                      "push %si;"
+                      "push %di;");
+    __asm__ volatile ("mov %0,%%ds;"
+                      "mov $0x0000,%%bx;"
+                      "int $0xA3;"
+                      :
+                      :"a" (l_seg)
+                      :"memory", "cc");
+    __asm__ volatile ("pop %di;"
+                      "pop %si;"
+                      "pop %bp;"
+                      "pop %es;"
+                      "pop %ds;");
+}
 
 static int
 VGA_open(PSD psd)
 {
+	unsigned char *lio_malloc;
+	unsigned char __far *lio_m;
+	unsigned int lio_m_seg;
+
+	unsigned long __far *intvec;
+	unsigned int __far *lioaddr;
+	unsigned int i;
+
 	/* setup operating mode from environment variable*/
 	if(getenv("EGAMODE"))
 		VGAMODE = FALSE;
@@ -100,13 +189,51 @@ VGA_open(PSD psd)
 	//ioctl(0, DCGET_GRAPH); /*continue if failed*/
 #endif
 
-#if HWINIT
-	/* enter graphics mode*/
-	ega_hwinit(psd);
-#else
-	/* init bios graphics mode*/
-	int10(VGAMODE? FNGR640x480: FNGR640x350, 0);
-#endif
+	intvec = (unsigned long __far *) _MK_FP(0, LIOINT<<2); /* interrupt vector for INT 0xA0 */
+	lioaddr = (unsigned int __far *) _MK_FP(LIOSEG, 6);   /* Starting Rom Address for INT 0xA0 handler */
+
+	// Set interrupt vector 0xA0 - 0xAF
+	for (i = 0; i < 16; i++) {
+		*intvec = (unsigned long) _MK_FP(LIOSEG, *lioaddr);
+		intvec++;
+		lioaddr += 2;
+	}
+
+	// Allocate memory for LIO
+	lio_malloc = malloc(LIO_M_SIZE);
+	lio_m = (unsigned char __far *) lio_malloc;
+
+	lio_m_seg = (unsigned int) ((((unsigned long) lio_m) >> 16) + ((((unsigned long) lio_m) & 0xFFFF) >> 4) + 1);
+
+	lio_m = (unsigned char __far *) (((unsigned long) lio_m_seg) << 16);
+
+	int_A0(lio_m_seg); // Init
+
+	lio_m[0] = 0x03; // Color 640x400
+	lio_m[1] = 0x00;
+	lio_m[2] = 0x00;
+	lio_m[3] = 0x01;
+	int_A1(lio_m_seg);
+
+	lio_m[0] = 0x00;
+	lio_m[1] = 0x00;
+	lio_m[2] = 0x00;
+	lio_m[3] = 0x00;
+	lio_m[4] = 0x7F; // 639
+	lio_m[5] = 0x02;
+	lio_m[6] = 0x8F; // 399
+	lio_m[7] = 0x01;
+	lio_m[8] = 0xFF;
+	lio_m[9] = 0xFF;
+	int_A2(lio_m_seg);
+
+	lio_m[0] = 0xFF;
+	lio_m[1] = 0xFF;
+	lio_m[2] = 0xFF;
+	lio_m[3] = 0x02; // 16 Color mode
+	int_A3(lio_m_seg);
+
+	free(lio_malloc);
 
 	/* init driver variables depending on ega/vga mode*/
 	psd->xres = 640;
@@ -144,8 +271,7 @@ VGA_close(PSD psd)
 #if HWINIT
 	ega_hwterm(psd);
 #else
-	/* init bios 80x25 text mode*/
-	int10(FNTEXT, 0);
+	outb(0x0C,0xA2);   // GDC Stop
 #endif
 }
 
