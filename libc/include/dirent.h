5b7f07e8846ea25db88aff55f1ca2f6024d7a204
Greg Haerr <greg@censoft.com>
2022-08-26 16:34:21 -0600

    [libc] Cleanup dirent struct and libs/programs that use it

--- libc/include/dirent.h
+++ libc/include/dirent.h
@@ -6,41 +6,36 @@
 #include <sys/types.h>
 #include __SYSINC__(dirent.h)
 
-#ifndef	MAXNAMLEN
-#define	MAXNAMLEN	255
-#endif
-
-/* Directory stream type.  */
+/* Directory stream type from opendir().  */
 typedef struct {
-  int dd_fd;			/* file descriptor */
-  int dd_loc;			/* offset in buffer */
-  int dd_size;			/* # of valid entries in buffer */
-  struct dirent *dd_buf;	/* -> directory buffer */
-} DIR;				/* stream data from opendir() */
+    int dd_fd;                  /* file descriptor */
+    int dd_loc;                 /* offset in buffer UNUSED */
+    int dd_size;                /* # of valid entries in buffer UNUSED */
+    struct dirent *dd_buf;      /* -> directory buffer */
+} DIR;
 
-typedef int (*__dir_select_fn_t) __P ((__const struct dirent *));
-
-typedef int (*__dir_compar_fn_t) __P ((
-                __const struct dirent * __const *,
-                __const struct dirent * __const *
-                ));
-
-extern DIR *opendir __P ((__const char *__name));
-extern int closedir __P ((DIR * __dirp));
-extern struct dirent *readdir __P ((DIR * __dirp));
-extern int _readdir(int fd, struct dirent *buf, int count);
-extern void rewinddir __P ((DIR * __dirp));
-
-extern void seekdir __P ((DIR * __dirp, off_t __pos));
-extern off_t telldir __P ((DIR * __dirp));
+DIR *opendir (const char *dname);
+int closedir(DIR * dirp);
+struct dirent *readdir(DIR * dirp);
+int _readdir(int fd, struct dirent *buf, int count);
+void rewinddir(DIR * dirp);
+void seekdir(DIR * dirp, off_t pos);
+off_t telldir(DIR * dirp);
 
 /* Scan the directory DIR, calling SELECT on each directory entry.
    Entries for which SELECT returns nonzero are individually malloc'd,
    sorted using qsort with CMP, and collected in a malloc'd array in
    *NAMELIST.  Returns the number of entries selected, or -1 on error.  */
-extern int scandir __P ((__const char *__dir,
-			 struct dirent ***__namelist,
-			 __dir_select_fn_t __select,
-			 __dir_compar_fn_t __compar));
+typedef int(*__dir_select_fn_t) (const struct dirent *);
+
+typedef int (*__dir_compar_fn_t) (
+                const struct dirent * const *,
+                const struct dirent * const *
+                );
+
+int scandir(const char *__dir,
+             struct dirent ***__namelist,
+             __dir_select_fn_t __select,
+             __dir_compar_fn_t __compar);
 
 #endif /* dirent.h  */
