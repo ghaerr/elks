fc9390b0fe1d2956188fea9d0280730704e42e24
Chuck Coffing <clc@alum.mit.edu>
2022-12-17 00:04:25 -0800

    Fix strncat

--- libc/string/strncat.c
+++ libc/string/strncat.c
@@ -5,7 +5,7 @@ strncat(char *d, const char *s, size_t l)
 {
    register char *s1=d+strlen(d), *s2;
 
-   s2 = memchr(s, l, 0);
+   s2 = memchr(s, 0, l);
    if( s2 )
       memcpy(s1, s, s2-s+1);
    else
