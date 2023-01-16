476e58d51d0f69b0d45c01b51a75ec1f7793d2b1
Greg Haerr <greg@censoft.com>
2023-01-15 18:00:10 -0700

    Update testsym and more complex instrument profiler

---
+++ elkscmd/debug/instrument.c
@@ -0,0 +1,106 @@
+/*
+ * ELKS instrumentation functions for ia16-elf-gcc
+ *
+ * June 2022 Greg Haerr
+ */
+#include <stdio.h>
+#include <string.h>
+#include "stacktrace.h"
+#include "syms.h"
+
+/* turn on for microcycle (CPU cycle/1000) timing info */
+#define HAS_RDTSC       1   /* has RDTSC instruction: requires 386+ CPU */
+
+static char ftrace;
+static int count;
+static unsigned int start_sp;
+static unsigned int max_stack;
+
+/* runs before main and rewrites argc/argv on stack if --ftrace found */
+__attribute__((no_instrument_function,cdecl,constructor(101)))
+static void checkargs(volatile int ac, char **av)
+{
+    char **avp = av + 1;
+
+    if (*avp && !strcmp(*avp, "--ftrace")) {
+        while (*avp) {
+            *avp = *(avp + 1);
+            avp++;
+        }
+        ftrace = 1;
+        ac--;
+    }
+    get_micro_count();      /* init timer base */
+}
+
+/* every function this function calls must also be noinstrument!! */
+void noinstrument __cyg_profile_func_enter_simple(void)
+{
+    if (!ftrace) return;
+
+#if 1
+    int **bp = __builtin_frame_address(0);  /* find BP on stack: BP, DI, SI, ret, arg1 */
+    int *calling_fn = __builtin_return_address(0);  /* return address */
+#else
+    int **bp = (int **)&arg1 - 4;   /* find BP on stack: BP, DI, SI, ret, arg1 */
+    int *calling_fn = bp[3];        /* return address */
+    assert(bp == __builtin_frame_address(0));
+    assert(calling_fn == __builtin_return_address(0));
+#endif
+    int i;
+    char callsite[32];
+
+    /* calc stack used */
+    if (count == 0) start_sp = (unsigned int)bp;
+    unsigned int stack_used = start_sp - (unsigned int)bp;
+    if (stack_used > max_stack) max_stack = stack_used;
+
+    //print_regs();
+    //print_stack(0xDEAD);
+
+    /* calc caller address */
+    i = calc_push_count(calling_fn);
+    if (i & BP_PUSHED) {            /* caller pushed BP */
+        bp = (int **)bp[0];         /* one level down to get caller BP */
+        i &= COUNT_MASK;
+    } else bp += 4;                 /* caller didn't push BP, skip past our ret addr */
+    if (i >= 0)
+        strcpy(callsite, sym_text_symbol(bp[i], 1));   /* return address of caller */
+    else
+        strcpy(callsite, "<unknown>");
+    for (i=0; i<count; i++)
+        putchar('|');
+    printf(">%s, from %s, stack %u/%u %lu ucycles\n",
+        sym_text_symbol(calling_fn, 0), callsite, stack_used, max_stack,
+        get_micro_count());
+    ++count;
+}
+
+void noinstrument __cyg_profile_func_exit_simple(void)
+{
+    if (ftrace)
+        --count;
+}
+
+/* return CPU cycles / 1000 via RDTSC instruction */
+unsigned long noinstrument get_micro_count(void)
+{
+#if HAS_RDTSC
+    static unsigned long long last_ts;
+
+    unsigned long long ts = rdtsc();    /* REQUIRES 386 CPU! */
+    unsigned long diff = (ts - last_ts) / 1000;
+    last_ts = ts;
+    return diff;
+#else
+    return 0;
+#endif
+}
+
+/***static char * noinstrument lltohexstr(unsigned long long val)
+{
+    static char buf[17];
+
+    sprintf(buf,"%08lx%08lx", (long)(val >> 32), (long)val);
+    return buf;
+}***/
--- elkscmd/debug/stacktrace.c
+++ elkscmd/debug/stacktrace.c
@@ -1,19 +1,12 @@
 /*
- * ELKS stack trace and instrumentation functions library for ia16-elf-gcc
+ * ELKS stack trace functions for ia16-elf-gcc
  *
  * June 2022 Greg Haerr
  */
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <assert.h>
 #include "stacktrace.h"
 #include "syms.h"
 
-#define PROFILING   1   /* =1 to include instrumentation functions */
 #define STACKCOLS   8   /* # of stack address columns */
 
 /* ia-16 C function stack layout (hi to low):
@@ -32,16 +25,11 @@
 +--------------+
 */
 
-#define BP_PUSHED   0x0100
-#define DI_PUSHED   0x0200
-#define SI_PUSHED   0x0400
-#define COUNT_MASK  0x0007
-
 /*
  * Return pushed word count and register bitmask by function at passed address,
  * used to traverse BP chain and display registers.
  */
-static int noinstrument calc_push_count(int *addr)
+int noinstrument calc_push_count(int *addr)
 {
     int *fn = sym_fn_start_address(addr);   /* get fn start from address */
     char *fp = (char *)fn;
@@ -102,69 +90,3 @@ void noinstrument print_stack(int arg1)
         }
     } while (++i < 15 && fn != 0 && addr != 0);
 }
-
-#if PROFILING
-static int count;
-static unsigned int start_sp;
-static unsigned int max_stack;
-
-/* every function this function calls must also be noinstrument!! */
-void noinstrument __cyg_profile_func_enter_simple(void)
-{
-#if 1
-    int **bp = __builtin_frame_address(0);  /* find BP on stack: BP, DI, SI, ret, arg1 */
-    int *calling_fn = __builtin_return_address(0);  /* return address */
-#else
-    int **bp = (int **)&arg1 - 4;   /* find BP on stack: BP, DI, SI, ret, arg1 */
-    int *calling_fn = bp[3];        /* return address */
-    assert(bp == __builtin_frame_address(0));
-    assert(calling_fn == __builtin_return_address(0));
-#endif
-    int i;
-    char callsite[32];
-
-    /* calc stack used */
-    if (count == 0) start_sp = (unsigned int)bp;
-    unsigned int stack_used = start_sp - (unsigned int)bp;
-    if (stack_used > max_stack) max_stack = stack_used;
-
-    //print_times();
-    //print_regs();
-    //print_stack(0xDEAD);
-
-    /* calc caller address */
-    i = calc_push_count(calling_fn);
-    if (i & BP_PUSHED) {            /* caller pushed BP */
-        bp = (int **)bp[0];         /* one level down to get caller BP */
-        i &= COUNT_MASK;
-    } else bp += 4;                 /* caller didn't push BP, skip past our ret addr */
-    if (i >= 0)
-        strcpy(callsite, sym_text_symbol(bp[i], 1));   /* return address of caller */
-    else
-        strcpy(callsite, "<unknown>");
-    for (i=0; i<count; i++)
-        putchar('|');
-    printf(">%s, from %s, stack %u/%u\n", sym_text_symbol(calling_fn, 0), callsite,
-        stack_used, max_stack);
-    ++count;
-}
-
-void noinstrument __cyg_profile_func_exit_simple(void)
-{
-    --count;
-}
-#endif /* PROFILING */
-
-#if LATER
-long long ts, last_ts, diff;
-
-void noinstrument print_times(void)
-{
-    ts = rdtsc();       /* REQUIRES 386 CPU! */
-    //diff = ts - last_ts;
-    diff = ts;
-    printf("time = %08lx%08lx, (%s),", (long)(diff >> 32), (long)diff, lltohexstr(diff));
-    printf("%s\n", ulltostr(diff, 16));
-    last_ts = ts;
-}
-#endif
