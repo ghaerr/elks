4fe3b53e3675f0f8bb6ab3b6786c5185a13ff5de
Greg Haerr <greg@censoft.com>
2023-03-04 11:45:22 -0700

    Standard formatting for passwd.c

--- elkscmd/sys_utils/passwd.c
+++ elkscmd/sys_utils/passwd.c
@@ -1,17 +1,16 @@
 /*
- * file:         passwd.c
- * description:  change user password
- * author:       Shane Kerr <kerr@wizard.net>
- * copyright:    1998 by Shane Kerr <kerr@wizard.net>, under terms of GPL
+ * file:         passwd.c description:  change user password author:
+ * Shane Kerr <kerr@wizard.net> copyright:    1998 by Shane Kerr
+ * <kerr@wizard.net>, under terms of GPL
  */
 
 /* todo:  make executable setuid on execute */
-/*        needs to check setuid bit in fs/exec.c of ELKS kernel */
+/* needs to check setuid bit in fs/exec.c of ELKS kernel */
 /* todo:  add a set of rules to make sure trivial passwords are not used */
 /* todo:  lock password file while updating */
-/*        need file locking first */
+/* need file locking first */
 /* todo:  use rename to do an atomic move of temporary file */
-/*        need rename that overwrites first */
+/* need rename that overwrites first */
 
 #include <unistd.h>
 #include <sys/types.h>
@@ -24,46 +23,45 @@
 #define MAX_FAILURE 5
 
 /* valid characters for a salt value */
-char salt_char[] =
-          "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./";
+char            salt_char [] =
+"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./";
 
-int main(argc,argv)
-int argc;
-char ** argv;
+int
+main(int argc, char **argv)
 {
-    struct passwd *pwd;
-    char *pbuf, salt[3];
-    char nbuf1[128], nbuf2[128];
-    time_t now;
-    int ch;
-    int failure_cnt;
-    FILE *passwd_fp;
-    char *tmp_fname;
-    int uid;
+    struct passwd  *pwd;
+    char           *pbuf, salt[3];
+    char            nbuf1  [128], nbuf2[128];
+    time_t          now;
+    int             ch;
+    int             failure_cnt;
+    FILE           *passwd_fp;
+    char           *tmp_fname;
+    int             uid;
 
     switch (argc) {
-        case 1:
-            pwd = getpwuid(getuid());
-            if (pwd == NULL) {
-                fprintf(stderr, "%s: Error reading password file\n", argv[0]);
-                return 1;
-            }
-            break;
-        case 2:
-            pwd = getpwnam(argv[1]);
-            if (pwd == NULL) {
-                fprintf(stderr, "%s: Unknown user %s\n", argv[0], argv[1]);
-                return 1;
-            }
-            if ((getuid() != 0) && (getuid() != pwd->pw_uid)) {
-                fprintf(stderr, "You may not change the password for %s.\n",
-                     argv[1]);
-                return 1;
-            }
-            break;
-        default:
-            fprintf(stderr, "usage: %s [ name ]\n", argv[0]);
+    case 1:
+        pwd = getpwuid(getuid());
+        if (pwd == NULL) {
+            fprintf(stderr, "%s: Error reading password file\n", argv[0]);
             return 1;
+        }
+        break;
+    case 2:
+        pwd = getpwnam(argv[1]);
+        if (pwd == NULL) {
+            fprintf(stderr, "%s: Unknown user %s\n", argv[0], argv[1]);
+            return 1;
+        }
+        if ((getuid() != 0) && (getuid() != pwd->pw_uid)) {
+            fprintf(stderr, "You may not change the password for %s.\n",
+                    argv[1]);
+            return 1;
+        }
+        break;
+    default:
+        fprintf(stderr, "usage: %s [ name ]\n", argv[0]);
+        return 1;
     }
 
     printf("Changing password for %s\n", pwd->pw_name);
@@ -74,13 +72,12 @@ char ** argv;
         salt[0] = pwd->pw_passwd[0];
         salt[1] = pwd->pw_passwd[1];
         salt[2] = 0;
-        if (strcmp(crypt(pbuf,salt),pwd->pw_passwd)) {
+        if (strcmp(crypt(pbuf, salt), pwd->pw_passwd)) {
             fprintf(stderr, "Incorrect password for %s.\n", pwd->pw_name);
             return 1;
         }
         memset(pbuf, 0, strlen(pbuf));
     }
-
     failure_cnt = 0;
     for (;;) {
         pbuf = getpass("New password:");
@@ -92,7 +89,7 @@ char ** argv;
         strcpy(nbuf2, pbuf);
         memset(pbuf, 0, strlen(pbuf));
         if (strcmp(nbuf1, nbuf2) == 0) {
-          /* I'm just going to use the LSB of time for salt */
+            /* I'm just going to use the LSB of time for salt */
             time(&now);
             ch = salt_char[now & 0x3F];
             salt[0] = ch;
@@ -100,7 +97,7 @@ char ** argv;
             salt[1] = ch;
             salt[2] = 0;
 
-          /* need to create a temporary file for the new password */
+            /* need to create a temporary file for the new password */
             tmp_fname = tmpnam(NULL);
             if (tmp_fname == NULL) {
                 perror("Error getting temporary file name");
@@ -111,15 +108,14 @@ char ** argv;
                 fprintf(stderr, "Error creating temporary password file\n");
                 return 1;
             }
-
-          /* save off our UID */
+            /* save off our UID */
             uid = pwd->pw_uid;
 
-          /* save entries to the new file */
+            /* save entries to the new file */
             pwd = getpwent();
             while (pwd != NULL) {
                 if (pwd->pw_uid == uid) {
-                        pwd->pw_passwd = crypt(nbuf1, salt);
+                    pwd->pw_passwd = crypt(nbuf1, salt);
                 }
                 if (putpwent(pwd, passwd_fp) == -1) {
                     perror("Error writing password file");
@@ -128,7 +124,7 @@ char ** argv;
                 pwd = getpwent();
             }
 
-          /* update the file and move it into position */
+            /* update the file and move it into position */
             fclose(passwd_fp);
             if (unlink("/etc/passwd") == -1) {
                 perror("Error removing old password file");
@@ -138,13 +134,12 @@ char ** argv;
                 perror("Error installing new password file");
                 return 1;
             }
-
-          /* celebrate our success */
+            /* celebrate our success */
             printf("Password changed.\n");
             return 0;
         }
         if (++failure_cnt >= MAX_FAILURE) {
-            fprintf(stderr, "The password for %s is unchanged.\n",pwd->pw_name);
+            fprintf(stderr, "The password for %s is unchanged.\n", pwd->pw_name);
             return 1;
         }
         fprintf(stderr, "They don't match; try again.\n");
