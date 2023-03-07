758e8f88c8271d783575f36e6ea572deef55c3d1
tkchia <tkchia@users.noreply.github.com>
2023-03-07 19:05:17 +0000

    [libc] Fix: abort () was calling kill ( ) with arguments reversed

--- libc/system/abort.c
+++ libc/system/abort.c
@@ -6,9 +6,9 @@ void
 abort(void)
 {
    signal (SIGABRT, SIG_DFL);
-   kill (SIGABRT, getpid ());  // first try
+   kill (getpid (), SIGABRT);  // first try
    pause ();  // system may just schedule
    signal (SIGKILL, SIG_DFL);
-   kill (SIGKILL, getpid ());  // second try
+   kill (getpid (), SIGKILL);  // second try
    _exit (255);  // third try
 }
