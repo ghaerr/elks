e9072a21e4e59e1781fa2d51ce9d3dcf1e638856
Greg Haerr <greg@censoft.com>
2023-03-08 19:31:46 -0700

    Cleanup test_sigfail.c

--- elkscmd/test/other/test_sigfail.c
+++ elkscmd/test/other/test_sigfail.c
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <time.h>
+#include <sys/select.h>
 
 #define ABORT   1
 #define INT     2
@@ -25,26 +27,35 @@ void sigint(int signo)
     signal(SIGINT, sigint);     // must reregister handler each time
 }
 
+void delay(long msecs)
+{
+    struct timeval tv;
+
+    tv.tv_sec = 0;
+    tv.tv_usec = msecs * 1000;
+    select(0, NULL, NULL, NULL, &tv);
+}
+
 int main(int argc, char **argv)
 {
     signal(SIGINT, sigint);
     signal(SIGABRT, sigabort);
     pid = getpid();
-    if (fork() == 0) {
+    if (vfork() == 0) {
         signal(SIGINT, SIG_IGN);
         signal(SIGABRT, SIG_IGN);
         for (;;) {
             status &= ~ABORT;
             kill(pid, SIGABRT);
             if (!(status & ABORT)) printf("ABORT failed\n");
-            if (getppid() == 1) exit(1);
+            if (getppid() == 1) _exit(1);
         }
     }
     for(;;) {
         status &= ~INT;
         kill(pid, SIGINT);
         if (!(status & INT)) printf("INT failed\n");
-        if (getppid() == 1) exit(1);
+        if (getppid() == 1) _exit(1);
     }
     return 0;
 }
