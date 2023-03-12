8009ff92ae4e5e6aaf8308f4582f19c9be42d626
Chuck Coffing <clc@alum.mit.edu>
2023-03-12 10:53:11 -0700

    Standard K&R 4-space formatting for some libc; no functional changes

--- libc/misc/atoi.c
+++ libc/misc/atoi.c
@@ -3,16 +3,16 @@
 
 int atoi(const char *s)
 {
-	int n = 0;
-	int neg = 0;
+    int n = 0;
+    int neg = 0;
 
-	while (*s == ' ' || *s == '\t')
-		s++;
-	switch (*s) {
-	case '-': neg = 1;
-	case '+': s++;
-	}
-	while ((unsigned) (*s - '0') <= 9u)
-		n = n * 10 + *s++ - '0';
-	return neg ? 0u - n : n;
+    while (*s == ' ' || *s == '\t')
+        s++;
+    switch (*s) {
+        case '-': neg = 1;
+        case '+': s++;
+    }
+    while ((unsigned) (*s - '0') <= 9u)
+        n = n * 10 + *s++ - '0';
+    return neg ? 0u - n : n;
 }
