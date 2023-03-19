0257fa40dc6e721e9b40d6b72e6e7ea3a239cbc7
Chuck Coffing <clc@alum.mit.edu>
2023-03-18 18:43:39 -0700

    [libc] fix NAME_MAX and add comments

--- libc/include/limits.h
+++ libc/include/limits.h
@@ -5,10 +5,13 @@
 #define PATH_MAX 128
 
 /* Maximum number of bytes in a filename, not including terminating null. */
-#define NAME_MAX 14
+/* copied from MAXNAMLEN in linuxmt/dirent.h */
+#define NAME_MAX 26
 
+/* copied from PIPE_BUFSIZ in linuxmt/config.h */
 #define PIPE_BUF 80
 
+/* copied from NR_OPEN in linuxmt/fs.h */
 #define OPEN_MAX 20
 
 #include_next <limits.h>
