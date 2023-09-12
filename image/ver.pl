7fb11456d48ac4ea5768b2bb5f55adbc9f90aaaf
Greg Haerr <greg@censoft.com>
2023-09-12 10:33:46 -0700

    [build] Update ELKS version to 0.8.0-dev

--- image/ver.pl
+++ image/ver.pl
@@ -2,10 +2,10 @@
 #Get the version from the kernel makefile
 
 while (<>) {
-        /^\s*VERSION\s*=\s*(\d+)/	&& ($ver = $1);
-        /^\s*PATCHLEVEL\s*=\s*(\d+)/	&& ($pat = $1);
-        /^\s*SUBLEVEL\s*=\s*(\d+)/	&& ($sub = $1);
-        /^\s*EXTRAVERSION\s*=\s*(.*)/	&& ($ext = $1);
+        /^\s*VERSION\s*=\s*(\d+)/       && ($ver = $1);
+        /^\s*PATCHLEVEL\s*=\s*(\d+)/    && ($pat = $1);
+        /^\s*SUBLEVEL\s*=\s*(\d+)/      && ($sub = $1);
+        /^\s*PRE\s*=\s*(.*)/            && ($pre = $1);
 }
 
-print "\nELKS ${ver}.${pat}.${sub}${ext}\n\n";
+print "\nELKS ${ver}.${pat}.${sub}${pre}\n\n";
