00dbefdb3a2e7a89931f1ff480a995dd787217e6
Chuck Coffing <clc@alum.mit.edu>
2023-12-07 19:22:59 -0800

    ELKS formatting; no code changes

--- libc/malloc/calloc.c
+++ libc/malloc/calloc.c
@@ -4,12 +4,12 @@
 void *
 calloc(unsigned int elm, unsigned int sz)
 {
-	register unsigned int v;
-	register void *ptr;
+    register unsigned int v;
+    register void *ptr;
 
-	ptr = malloc(v = elm * sz);
-	if(ptr)
-		memset(ptr, 0, v);
+    ptr = malloc(v = elm * sz);
+    if (ptr)
+        memset(ptr, 0, v);
 
-	return ptr;
+    return ptr;
 }
