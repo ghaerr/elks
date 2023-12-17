0d802d35cd31b7be9250ac3a5aa8acf2be217278
Chuck Coffing <clc@alum.mit.edu>
2023-12-17 07:58:14 -0800

    ELKS formatting; no functional changes

--- elkscmd/file_utils/touch.c
+++ elkscmd/file_utils/touch.c
@@ -8,34 +8,35 @@
 #include <utime.h>
 #include "futils.h"
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
-	int i, ncreate = 0;
-	int err = 0;
-	struct stat sbuf;
+    int i, ncreate = 0;
+    int err = 0;
+    struct stat sbuf;
 
-	if (argc < 2) {
-		errmsg("usage: touch file [...]\n");
-		return 1;
-	}
-	if ((argv[1][0] == '-') && (argv[1][1] == 'c'))
-		ncreate = 1;
+    if (argc < 2) {
+        errmsg("usage: touch file [...]\n");
+        return 1;
+    }
+    if ((argv[1][0] == '-') && (argv[1][1] == 'c'))
+        ncreate = 1;
 
-	for (i = ncreate + 1; i < argc; i++) {
-		if (argv[i][0] != '-') {
-			if (stat(argv[i], &sbuf)) {
-				if (!ncreate) {
-					int fd = creat(argv[i], 0666);
-					if (fd < 0) {
-						errstr(argv[i]);
-						errmsg(": cannot create file\n");
-						err = 1;
-					}
-					else close(fd);
-				}
-			} else
-				err |= utime(argv[i], NULL);
-		}
-	}
-	return (err ? 1 : 0);
+    for (i = ncreate + 1; i < argc; i++) {
+        if (argv[i][0] != '-') {
+            if (stat(argv[i], &sbuf)) {
+                if (!ncreate) {
+                    int fd = creat(argv[i], 0666);
+                    if (fd < 0) {
+                        errstr(argv[i]);
+                        errmsg(": cannot create file\n");
+                        err = 1;
+                    } else
+                        close(fd);
+                }
+            } else
+                err |= utime(argv[i], NULL);
+        }
+    }
+    return (err ? 1 : 0);
 }
