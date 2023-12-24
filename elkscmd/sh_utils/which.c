21d3dcd89b83c95f0a74978d9af8637a36fae565
Chuck Coffing <clc@alum.mit.edu>
2023-12-24 10:11:32 -0800

    Don't use stdio for which

--- elkscmd/sh_utils/which.c
+++ elkscmd/sh_utils/which.c
@@ -1,8 +1,9 @@
 #include <limits.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#define errstr(str) write(STDERR_FILENO, str, strlen(str))
 
 int
 main(int argc, char **argv)
@@ -12,7 +13,7 @@ main(int argc, char **argv)
     int r = 1;
 
     if (argc < 2) {
-        puts("Usage: which cmd [...]");
+        errstr("Usage: which cmd [...]\n");
         return 1;
     }
     if ((envpath = getenv("PATH")) == 0)
@@ -20,7 +21,7 @@ main(int argc, char **argv)
 
     argv[argc] = 0;
     for (argv++; *argv; argv++) {
-        const size_t file_len = strlen(*argv);
+        const size_t file_len = strlen(*argv) + 1;
         const char *start = envpath;
         const char *end = start;
 
@@ -28,13 +29,15 @@ main(int argc, char **argv)
             while (*end && *end != ':')
                 ++end;
             size_t path_len = end - start;
-            if (path_len + 1 + file_len + 1 <= sizeof(path)) {
+            if (path_len + 1 + file_len <= sizeof(path)) {
                 memcpy(path, start, path_len);
-                path[path_len] = '/';
-                strcpy(path + path_len + 1, *argv);
+                path[path_len++] = '/';
+                memcpy(path + path_len, *argv, file_len);
+                path_len += file_len;
 
                 if (access(path, F_OK) == 0) {
-                    puts(path);
+                    write(STDOUT_FILENO, path, path_len);
+                    write(STDOUT_FILENO, "\n", 1);
                     r = 0;
                     break;
                 }
