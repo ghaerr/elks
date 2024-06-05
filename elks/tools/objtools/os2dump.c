369fe35d702be040259d91dcbde613cf6939b3d2
Greg Haerr <greg@censoft.com>
2024-06-05 13:54:53 -0600

    Remove os2dump.h header file

--- elks/tools/objtools/os2dump.c
+++ elks/tools/objtools/os2dump.c
@@ -28,7 +28,139 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#include "os2dump.h"
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t sint8;
+typedef int16_t sint16;
+typedef int32_t sint32;
+typedef int64_t sint64;
+
+typedef unsigned int uint;
+
+#pragma pack(push, 1)
+typedef struct LxHeader
+{
+    uint8 magic_l;
+    uint8 magic_x;
+    uint8 byte_order;
+    uint8 word_order;
+    uint32 lx_version;
+    uint16 cpu_type;
+    uint16 os_type;
+    uint32 module_version;
+    uint32 module_flags;
+    uint32 module_num_pages;
+    uint32 eip_object;
+    uint32 eip;
+    uint32 esp_object;
+    uint32 esp;
+    uint32 page_size;
+    uint32 page_offset_shift;
+    uint32 fixup_section_size;
+    uint32 fixup_section_checksum;
+    uint32 loader_section_size;
+    uint32 loader_section_checksum;
+    uint32 object_table_offset;
+    uint32 module_num_objects;
+    uint32 object_page_table_offset;
+    uint32 object_iter_pages_offset;
+    uint32 resource_table_offset;
+    uint32 num_resource_table_entries;
+    uint32 resident_name_table_offset;
+    uint32 entry_table_offset;
+    uint32 module_directives_offset;
+    uint32 num_module_directives;
+    uint32 fixup_page_table_offset;
+    uint32 fixup_record_table_offset;
+    uint32 import_module_table_offset;
+    uint32 num_import_mod_entries;
+    uint32 import_proc_table_offset;
+    uint32 per_page_checksum_offset;
+    uint32 data_pages_offset;
+    uint32 num_preload_pages;
+    uint32 non_resident_name_table_offset;
+    uint32 non_resident_name_table_len;
+    uint32 non_resident_name_table_checksum;
+    uint32 auto_ds_object_num;
+    uint32 debug_info_offset;
+    uint32 debug_info_len;
+    uint32 num_instance_preload;
+    uint32 num_instance_demand;
+    uint32 heapsize;
+} LxHeader;
+
+typedef struct LxObjectTableEntry
+{
+    uint32 virtual_size;
+    uint32 reloc_base_addr;
+    uint32 object_flags;
+    uint32 page_table_index;
+    uint32 num_page_table_entries;
+    uint32 reserved;
+} LxObjectTableEntry;
+
+typedef struct LxObjectPageTableEntry
+{
+    uint32 page_data_offset;
+    uint16 data_size;
+    uint16 flags;
+} LxObjectPageTableEntry;
+
+typedef struct LxResourceTableEntry
+{
+    uint16 type_id;
+    uint16 name_id;
+    uint32 resource_size;
+    uint16 object;
+    uint32 offset;
+} LxResourceTableEntry;
+
+typedef struct NeHeader
+{
+    uint8 magic_n;
+    uint8 magic_e;
+    uint8 linker_version;
+    uint8 linker_revision;
+    uint16 entry_table_offset;
+    uint16 entry_table_size;
+    uint32 crc32;
+    uint16 module_flags;
+    uint16 auto_data_segment;
+    uint16 dynamic_heap_size;
+    uint16 stack_size;
+    uint16 reg_ip;
+    uint16 reg_cs;
+    uint16 reg_sp;
+    uint16 reg_ss;
+    uint16 num_segment_table_entries;
+    uint16 num_module_ref_table_entries;
+    uint16 non_resident_name_table_size;
+    uint16 segment_table_offset;
+    uint16 resource_table_offset;
+    uint16 resident_name_table_offset;
+    uint16 module_reference_table_offset;
+    uint16 imported_names_table_offset;
+    uint32 non_resident_name_table_offset;
+    uint16 num_movable_entries;
+    uint16 sector_alignment_shift_count;
+    uint16 num_resource_entries;
+    uint8 exe_type;
+    uint8 os2_exe_flags;
+    uint8 reserved[8];
+} NeHeader;
+
+typedef struct NeSegmentTableEntry
+{
+    uint16 offset;
+    uint16 size;
+    uint16 segment_flags;
+    uint16 minimum_allocation;
+} NeSegmentTableEntry;
+
+#pragma pack(pop)
 
 // !!! FIXME: some cut-and-paste with lx_loader.c ...
 
