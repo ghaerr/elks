30627cdb56162ac7c62228f91d6f37acb6b408d7
tyama501 <61556504+tyama501@users.noreply.github.com>
2024-06-15 03:22:56 +0900

    [owc] Add unsigned to __pseg

--- libc/watcom/syscall/fmemalloc.c
+++ libc/watcom/syscall/fmemalloc.c
@@ -10,6 +10,6 @@
 int _fmemalloc( int __paras, seg_t *__pseg )
 {
     sys_setseg(__pseg);
-    syscall_res res = sys_call2( SYS_fmemalloc, __paras, __pseg);
+    syscall_res res = sys_call2( SYS_fmemalloc, __paras, (unsigned)__pseg);
     __syscall_return( int, res );
 }
