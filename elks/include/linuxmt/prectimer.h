69b1b0305f917cd5a6a44d2895155126089ec6e8
Greg Haerr <greg@censoft.com>
2024-08-07 16:31:11 -0700

    [kernel] Update examples for precision timer

--- elks/include/linuxmt/prectimer.h
+++ elks/include/linuxmt/prectimer.h
@@ -3,6 +3,9 @@
  * 2 Aug 2024 Greg Haerr
  */
 
+#define CONFIG_PREC_TIMER   0   /* =1 to include %k precision timer printk format */
+#define TIMER_TEST          0   /* =1 to include timer_*() test routines */
+
 /* all routines return pticks = 0.8381 usecs */
 unsigned int get_time_10ms(void);   /* < 10ms measurements */
 unsigned int get_time_50ms(void);   /* < 50ms measurements */
