f86877c8dd889af1ff5c1d15ba269978ce31a704
Greg Haerr <greg@censoft.com>
2024-09-03 18:26:03 -0600

    [bootopts] Add heap= option for dynamic sizing of kernel heap

--- elks/include/arch/system.h
+++ elks/include/arch/system.h
@@ -4,9 +4,11 @@
 #include <linuxmt/types.h>
 #include <linuxmt/init.h>
 
-extern byte_t sys_caps;		/* system capabilities bits*/
+extern byte_t sys_caps;     /* system capabilities bits*/
+extern seg_t membase;       /* start and end segment of available main memory */
+extern seg_t memend;
 
-extern void INITPROC setup_arch(seg_t *,seg_t *);
+extern unsigned int INITPROC setup_arch(void);
 extern void hard_reset_now(void);
 extern void apm_shutdown_now(void);
 
