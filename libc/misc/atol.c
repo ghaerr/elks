f14dee3199e2a3e28a416b958214611072b63777
Greg Haerr <greg@censoft.com>
2024-10-14 22:26:48 -0600

    Clean up atoi.c atol.c

--- libc/misc/atol.c
+++ libc/misc/atol.c
@@ -1,18 +1,17 @@
 #include <stdlib.h>
-#include <ctype.h>
 
 long atol(const char *s)
 {
-	long n = 0;
-	int neg = 0;
+    long n = 0;
+    int neg = 0;
 
-	while (*s == ' ' || *s == '\t')
-		s++;
-	switch (*s) {
-	case '-': neg = 1;
-	case '+': s++;
-	}
-	while ((unsigned) (*s - '0') <= 9u)
-		n = n * 10 + *s++ - '0';
-	return neg ? 0u - n : n;
+    while (*s == ' ' || *s == '\t')
+        s++;
+    switch (*s) {
+    case '-': neg = 1;
+    case '+': s++;
+    }
+    while ((unsigned) (*s - '0') <= 9u)
+        n = n * 10 + *s++ - '0';
+    return neg ? 0u - n : n;
 }
