a31af983e4fb0b24680c702450a2d579d808a6cc
Greg Haerr <greg@censoft.com>
2024-11-15 17:55:24 -0800

    [libc] Cleanup old BCC ASM from .c files

--- libc/string/memchr.c
+++ libc/string/memchr.c
@@ -1,48 +1,39 @@
 #include <string.h>
 
+#ifdef BCC_ASM
+#asm
+  mov   bx,sp
+  push  di
+  push  es
+  push  ds
+  pop   es
+  cld
+  mov   di,[bx+2]
+  mov   ax,[bx+4]
+  mov   cx,[bx+6]
+  test  cx,cx
+  je    is_z    ! Zero length, do not find.
+  repne         ! Scan
+  scasb
+  jne   is_z    ! Not found, ret zero
+  dec   di      ! Adjust ptr
+  mov   ax,di   ! return
+  jmp   xit
+is_z:
+  xor   ax,ax
+xit:
+  pop   es
+  pop   di
+  ret
+#endif
+
 void *
 memchr(const void * str, int c, size_t l)
 {
-#ifdef BCC_ASM
-#asm
-  mov	bx,sp
-  push	di
-
-#ifdef PARANOID
-  push	es
-  push	ds	; Im not sure if this is needed, so just in case.
-  pop	es
-  cld
-#endif
-
-  mov	di,[bx+2]
-  mov	ax,[bx+4]
-  mov	cx,[bx+6]
-  test	cx,cx
-  je	is_z	! Zero length, do not find.
-
-  repne		! Scan
-  scasb
-  jne	is_z	! Not found, ret zero
-  dec	di	! Adjust ptr
-  mov	ax,di	! return
-  jmp	xit
-is_z:
-  xor	ax,ax
-xit:
-
-#ifdef PARANOID
-  pop	es
-#endif
-  pop	di
-#endasm
-#else /* ifdef BCC_ASM */
-   register unsigned char *p=(unsigned char *)str;
-   while(l-- > 0)
-   {
-      if(*p == c) return p;
+   unsigned char *p = (unsigned char *)str;
+   while (l-- > 0) {
+      if (*p == c) return p;
       p++;
    }
    return 0;
-#endif /* ifdef BCC_ASM */
 }
