bc325ce3d53e396da506ee23d6f547f3356bcdaa
Greg Haerr <greg@censoft.com>
2025-03-24 09:43:43 -0600

    [ktcp] Fix duplicate assignment statement

--- elkscmd/ktcp/tcp_cb.c
+++ elkscmd/ktcp/tcp_cb.c
@@ -83,13 +83,11 @@ struct tcpcb_list_s *tcpcb_new(int bufsize)
 	n->next = tcpcbs;
 	n->prev = NULL;
 	tcpcbs->prev = n;
-	tcpcbs = n;
     } else {
-	tcpcbs = n;
 	n->next = NULL;
 	n->prev = NULL;
-	tcpcbs = n;
     }
+    tcpcbs = n;
     tcpcb_num++;	/* for netstat*/
 
     return n;
