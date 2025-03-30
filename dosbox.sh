3803412417160d606eb860b20cef8bec273896e0
Greg Haerr <greg@censoft.com>
2025-03-30 12:47:50 -0600

    Get PC-98 xms size from BIOS area

--- dosbox.sh
+++ dosbox.sh
@@ -2,10 +2,10 @@
 #
 
 # 1232k image from './buildimages.sh fast'
-exec ./dosbox-x-sdl2 -set machine=pc98 -set "dosbox quit warning=false" -fastlaunch -c "boot image/fd1232-pc98.img"
+#exec ./dosbox-x-sdl2 -set machine=pc98 -set "dosbox quit warning=false" -fastlaunch -c "boot image/fd1232-pc98.img"
 
 # just-built 1232k image
-#exec ./dosbox-x-sdl2 -set machine=pc98 -set "dosbox quit warning=false" -fastlaunch -c "boot image/fd1232.img"
+exec ./dosbox-x-sdl2 -set machine=pc98 -set "dosbox quit warning=false" -fastlaunch -c "boot image/fd1232.img"
 
 # 1440k image
 #exec ./dosbox-x-sdl2 -set machine=pc98 -set "dosbox quit warning=false" -fastlaunch -c "boot image/fd1440-pc98.img"
