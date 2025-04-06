a13b3a537c19617264c118a4223a696efefafe5a
Greg Haerr <greg@censoft.com>
2025-04-05 17:00:59 -0700

    [cmds] Fix vga_drawhline in Paint

--- elkscmd/gui/vga-c86.s
+++ elkscmd/gui/vga-c86.s
@@ -48,7 +48,6 @@ _vga_init:
 ;   static unsigned char mask[8] = { 0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01 };
 ;   //set_op(0);
 ;   set_color(c);
-;   select_mask();
 ;   set_mask(mask[x&7]);
 ;
         .global  _vga_drawpixel
@@ -113,7 +112,6 @@ _vga_drawpixel:
 ;   set_color(c);
 ;   //set_op(0);
 ;   char far *dst = SCREENBASE + x1 / 8 + y * BYTESPERLN;
-;   select_mask();
 ;   if (x1 / 8 == x2 / 8) {
 ;       set_mask((0xff >> (x1 & 7)) & (0xff << (7 - (x2 & 7))));
 ;       *dst |= 1;
@@ -265,7 +263,6 @@ L44:    mov     ah, bl          ; AH := bit mask for last byte
 ; C version:
 ;   //set_op(0);
 ;   set_color(c);
-;   select_mask();
 ;   set_mask(mask[x&7]);
 ;   char far *dst = SCREENBASE + x / 8 + y1 * BYTESPERLN;
 ;   char far *last = SCREENBASE + x / 8 + y2 * BYTESPERLN;
