4f59d3274bc2e494fe4979a7ace79de945a2f1e9
Greg Haerr <greg@censoft.com>
2025-09-16 18:08:33 -0700

    [libc] Add DEBUG_PORT= environ var for __dprintf output

--- elkscmd/inet/telnet/build_telnetc.sh
+++ elkscmd/inet/telnet/build_telnetc.sh
@@ -5,5 +5,5 @@
 CFLAGS="-O2 -pipe -Wall"
 
 gcc $CFLAGS -c -o ttn_conf.o ttn_conf.c
-gcc $CFLAGS -c -o ttn.o ttn.c
-gcc $CFLAGS ttn_conf.o ttn.o -o telnetc
+gcc $CFLAGS -c -o telnet.o telnet.c
+gcc $CFLAGS ttn_conf.o telnet.o -o telnetc
