961a4490e2fbe41ca51be04f7407c4344f28ede1
Greg Haerr <greg@censoft.com>
2025-10-10 19:18:17 -0600

    Fix typo

--- elkscmd/disk_utils/mkfat.c
+++ elkscmd/disk_utils/mkfat.c
@@ -55,7 +55,7 @@ struct sec_per_clus_table
 struct sec_per_clus_table _cluster_size_table16[] =
 {
     { 4084L,   1},   // 2MB - 512b
-    { 65525L,  2},   // 64MB - 1K (was 32680/16MB)
+    { 65524L,  2},   // 64MB - 1K (was 32680/16MB)
     { 262144L, 4},   // 128MB - 2K
     { 524288L, 8},   // 256MB - 4K
     { 1048576L, 16}, // 512MB - 8K
