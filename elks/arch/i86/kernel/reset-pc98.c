1f6afb05add5cf68e8b1523a01342cc61c194651
tyama501 <61556504+tyama501@users.noreply.github.com>
2025-10-22 22:58:38 +0900

    [pc98_reboot] Add reset-pc98.c and moved the reset function from reset-stub.c

---
+++ elks/arch/i86/kernel/reset-pc98.c
@@ -0,0 +1,35 @@
+#include <arch/system.h>
+
+/*
+ * This function gets called by the keyboard interrupt handler.
+ * As it's called within an interrupt, it may NOT sync.
+ */
+void ctrl_alt_del(void)
+{
+}
+
+void hard_reset_now(void)
+{
+    asm("mov $0x37,%dx\n\t"
+        "mov $0xb,%al\n\t"
+        "out %al,%dx\n\t"
+        "mov $0xf,%al\n\t"
+        "out %al,%dx\n\t"
+        "mov $0xf0,%dx\n\t"
+        "in  %dx,%al\n\t"
+        "and $2,%al\n\t"
+        "cmp $2,%al\n\t"
+        "je V30\n\t"
+        "mov $0,%al\n\t"
+        "out %al,%dx\n\t"
+        "ljmp $0xFFFF,$0\n\t"
+"V30:\n\t"
+        "mov $7,%al\n\t"
+        "out %al,%dx\n\t"
+        "ljmp $0xFFFF,$0\n\t"
+    );
+}
+
+void apm_shutdown_now(void)
+{
+}
