eb95084a41ac59a6ba4f63e4eef9ab9989c53175
drachen6jp <drachen6jp@yahoo.co.jp>
2025-10-22 20:38:23 +0900

    hard_reset for PC-98 type

--- elks/arch/i86/kernel/reset-stubs.c
+++ elks/arch/i86/kernel/reset-stubs.c
@@ -10,6 +10,27 @@ void ctrl_alt_del(void)
 
 void hard_reset_now(void)
 {
+#if defined CONFIG_ARCH_PC98
+    asm("mov $0x37,%dx\n\t"
+        "mov $0xb,%al\n\t"
+        "out %al,%dx\n\t"
+        "mov $0xf,%al\n\t"
+        "out %al,%dx\n\t"
+        "mov $0xf0,%dx\n\t"
+        "in  %dx,%al\n\t"
+        "and $2,%al\n\t"
+        "cmp $2,%al\n\t"
+        "je V30\n\t"
+        "mov $0,%al\n\t"
+        "out %al,%dx\n\t"
+        "ljmp $0xFFFF,$0\n\t"
+"V30:\n\t"
+        "mov $7,%al\n\t"
+        "out %al,%dx\n\t"
+        "ljmp $0xFFFF,$0\n\t"
+    );
+
+#endif
 }
 
 void apm_shutdown_now(void)
