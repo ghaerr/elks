9137957f881b81a639f9061ec868976e16a9603b
tyama501 <61556504+tyama501@users.noreply.github.com>
2025-11-01 01:12:07 +0900

    [bios] Removed ifdef CONFIG_IMG_FD1232 (again)

--- elks/arch/i86/drivers/block/bios.c
+++ elks/arch/i86/drivers/block/bios.c
@@ -47,11 +47,7 @@ struct drive_infot fd_types[] = {   /* AT/PS2 BIOS reported floppy formats*/
 #ifdef CONFIG_ARCH_PC98
 unsigned char bios_drive_map[MAX_DRIVES] = {
     0xA0, 0xA1, 0xA2, 0xA3,             /* hda, hdb */
-#ifdef CONFIG_IMG_FD1232
     0x90, 0x91, 0x92, 0x93              /* fd0, fd1 */
-#else
-    0x30, 0x31, 0x32, 0x33              /* fd0, fd1 */
-#endif
 };
 #else
 unsigned char bios_drive_map[MAX_DRIVES] = {
@@ -291,17 +287,10 @@ int INITPROC bios_getfdinfo(struct drive_infot *drivep)
     int ndrives = FD_DRIVES;
 
 #ifdef CONFIG_ARCH_PC98
-#if defined(CONFIG_IMG_FD1232)
     drivep[0] = fd_types[FD1232];
     drivep[1] = fd_types[FD1232];
     drivep[2] = fd_types[FD1232];
     drivep[3] = fd_types[FD1232];
-#else
-    drivep[0] = fd_types[FD1440];
-    drivep[1] = fd_types[FD1440];
-    drivep[2] = fd_types[FD1440];
-    drivep[3] = fd_types[FD1440];
-#endif
 #endif
 
 #ifdef CONFIG_ARCH_IBMPC
@@ -322,13 +311,9 @@ int INITPROC bios_getfdinfo(struct drive_infot *drivep)
 #ifdef CONFIG_ARCH_PC98
     for (drive = 0; drive < 4; drive++) {
         if (peekb(0x55C,0) & (1 << drive)) {
-#ifdef CONFIG_IMG_FD1232
             bios_drive_map[DRIVE_FD0 + drive] = drive + 0x90;
             *drivep = fd_types[FD1232];
-#else
-            bios_drive_map[DRIVE_FD0 + drive] = drive + 0x30;
-            *drivep = fd_types[FD1440];
-#endif
+
             ndrives++;  /* floppy drive count*/
             drivep++;
         }
