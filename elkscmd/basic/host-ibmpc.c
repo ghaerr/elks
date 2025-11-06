478b2d24aa0461eee07e28bf6751e87622149c3b
Greg Haerr <greg@censoft.com>
2025-11-06 16:12:53 -0700

    [cmds] Extend Basic MODE command for CGA, EGA and EGA

--- elkscmd/basic/host-ibmpc.c
+++ elkscmd/basic/host-ibmpc.c
@@ -8,10 +8,18 @@
 #include "host.h"
 #include "basic.h"
 
-/* supported graphics modes */
-#define VGA_640x350x16      0x10        /* 640x350 16 color/4bpp */
-#define VGA_640x480x16      0x12        /* 640x480 16 color/4bpp */
+/* supported BIOS video modes */
 #define TEXT_MODE           0x03        /* 80x25 text mode */
+#define CGA_320x200x4       0x04        /* 320x200 4 color/2bpp */
+#define EGA_640x350x16      0x10        /* 640x350 16 color/4bpp */
+#define VGA_640x480x16      0x12        /* 640x480 16 color/4bpp */
+
+/* MODE n values */
+#define TEXT    0
+#define DEFAULT 1
+#define VGA     2
+#define EGA     3
+#define CGA     4
 
 /* graphics context */
 struct gc {
@@ -57,32 +65,56 @@ static void mode_reset(void)
 
 void host_mode(int mode)
 {
-    gmode = mode;
-
-    if (gmode && !exit_on) {
+    if (mode && !exit_on) {
         atexit(mode_reset);
         exit_on = 1;
     }
 
-    if (gmode) {
-        if (getenv("EGAMODE") != NULL) {
-            gmode = VGA_640x350x16;
-            MAX_Y = 350 - 1;
-        } else {
-            gmode = VGA_640x480x16;
-            MAX_Y = 480 - 1;
-        }
-        int_10(gmode, 0, 0, 0);
-    } else
-        int_10(TEXT_MODE, 0, 0, 0);
+    if (mode == DEFAULT)
+        mode = getenv("EGAMODE")? EGA: VGA;
+    switch (mode) {
+    case TEXT:
+        gmode = TEXT_MODE;
+        break;
+    case VGA:
+        gmode = VGA_640x480x16;
+        MAX_Y = 480 - 1;
+        break;
+    case EGA:
+        gmode = EGA_640x350x16;
+        MAX_Y = 350 - 1;
+        break;
+    case CGA:
+        gmode = CGA_320x200x4;
+        MAX_Y = 200 - 1;
+        break;
+    default:
+        gmode = mode;       /* MODE > 3 uses actual mode passed */
+        MAX_Y = 0;          /* but (0,0) will be upper left */
+        break;
+    }
+    int_10(gmode, 0, 0, 0);
 }
 
 void host_cls(void)
 {
-    if (gmode)
-        fmemsetw(0, 0xA000, 0, 19200);  /* 640*480/8 bytes VGA ram */
-    else
+    switch (gmode) {
+    case 0:
         fprintf(outfile, "\033[H\033[2J");
+        break;
+    case VGA_640x480x16:                /* 640x480/8 = 38,400 bytes */
+        fmemsetw(0, 0xA000, 0, 19200);
+        break;
+    case EGA_640x350x16:                /* 640x350/8 = 28,000 bytes */
+        fmemsetw(0, 0xA000, 0, 14000);
+        break;
+    case CGA_320x200x4:                 /* 320x200x4 = 16,000 RAM, two banks of 8000 */
+        fmemsetw(0, 0xB800, 0, 4000);   /* CGA bank 0 */
+        fmemsetw(0, 0xBA00, 0, 4000);   /* CGA bank 1 */
+        break;
+    default:
+        break;
+    }
 }
 
 void host_color(int fg, int bg)
@@ -126,7 +158,7 @@ static void draw_line(int x1, int y1, int x2, int y2)
 void host_plot(int x, int y)
 {
     if (gmode) {
-        y = MAX_Y - y;
+        if (MAX_Y) y = MAX_Y - y;
 
         draw_point(x, y);
 
@@ -138,7 +170,7 @@ void host_plot(int x, int y)
 void host_draw(int x, int y)
 {
     if (gmode) {
-        y = MAX_Y - y;
+        if (MAX_Y) y = MAX_Y - y;
         draw_line(gc.x, gc.y, x, y);
         gc.x = x;
         gc.y = y;
