98eaaf168b0c9bd0ddf07f709551a143f4961aed
Greg Haerr <greg@censoft.com>
2025-12-15 11:36:58 -0800

    Cleanup

--- elks/init/main.c
+++ elks/init/main.c
@@ -17,7 +17,6 @@
 #include <linuxmt/prectimer.h>
 #include <linuxmt/debug.h>
 #include <arch/segment.h>
-#include <arch/asm-offsets.h>
 #include <arch/ports.h>
 #include <arch/irq.h>
 #include <arch/io.h>
@@ -141,7 +140,7 @@ void start_kernel(void)
      * since it overflows into relatively unused areas of the idle task's task_struct.
      */
     setsp(&idle_task->t_kstack[IDLESTACK_BYTES/2]); /* change to small idle task stack */
-    hexdump(idle_task->t_kstack, kernel_ds, IDLESTACK_BYTES, 1);
+    //hexdump(idle_task->t_kstack, kernel_ds, IDLESTACK_BYTES, 1);
 
     /*
      * In the call to schedule below, the init_task function will run, which
