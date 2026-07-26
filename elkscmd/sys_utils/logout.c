7b919d9ea210e71eb008b043602bcac91f39e0eb
derekjohansen <derekjohansen1@gmail.com>
2026-07-26 13:18:23 -0600

    update per review comments

--- elkscmd/sys_utils/logout.c
+++ elkscmd/sys_utils/logout.c
@@ -1,3 +1,13 @@
+/*
+ * logout - end a user session
+ *
+ * Usage: logout [username]
+ *
+ * Without arguments, sends SIGHUP to current process group.
+ * With username (root only), finds the user's login shell
+ * and sends SIGHUP to its process group.
+ */
+
 #define __KERNEL__
 #include <linuxmt/ntty.h>
 #undef __KERNEL__
@@ -5,15 +15,15 @@
 #include <linuxmt/mm.h>
 #include <linuxmt/mem.h>
 #include <linuxmt/sched.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <pwd.h>
 #include <signal.h>
 
-static int memread(int fd, word_t off, word_t seg, void *buf, int size)
+#define errmsg(str) write(STDERR_FILENO, str, sizeof(str) - 1)
+
+static int memread(int fd, unsigned int off, unsigned int seg, void *buf, int size)
 {
     if (lseek(fd, _MK_LP(seg, off), SEEK_SET) == -1)
         return -1;
@@ -26,73 +36,58 @@ int main(int argc, char **argv)
     unsigned int j, ds, off;
     struct task_struct task_table;
     struct passwd *pwent;
-    uid_t target_uid = (uid_t)-1;
-    int found = 0;
+    uid_t target_uid;
     int maxtasks;
 
     if (argc > 2) {
-        fprintf(stderr, "Usage: logout [username]\n");
+        errmsg("Usage: logout [username]\n");
         return 1;
     }
 
-    if (argc == 2) {
-        if (getuid() != 0) {
-            fprintf(stderr, "logout: only root can logout other users\n");
-            return 1;
-        }
-        pwent = getpwnam(argv[1]);
-        if (!pwent) {
-            fprintf(stderr, "logout: unknown user '%s'\n", argv[1]);
-            return 1;
-        }
-        target_uid = pwent->pw_uid;
+    /* no args: logout self - send SIGHUP to our process group */
+    if (argc == 1) {
+        kill(0, SIGHUP);
+        return 0;
     }
 
+    if (getuid() != 0) {
+        errmsg("logout: must be root\n");
+        return 1;
+    }
+
+    pwent = getpwnam(argv[1]);
+    if (!pwent) {
+        errmsg("logout: unknown user\n");
+        return 1;
+    }
+    target_uid = pwent->pw_uid;
+
     if ((fd = open("/dev/kmem", O_RDONLY|O_ALT)) < 0) {
-        fprintf(stderr, "logout: cannot open /dev/kmem\n");
+        errmsg("logout: cannot open /dev/kmem\n");
         return 1;
     }
     if (ioctl(fd, MEM_GETDS, &ds) < 0 ||
-        ioctl(fd, MEM_GETMAXTASKS, &maxtasks) < 0) {
-        fprintf(stderr, "logout: cannot get kernel info\n");
-        close(fd);
-        return 1;
-    }
-    if (ioctl(fd, MEM_GETTASK, &off) < 0) {
-        fprintf(stderr, "logout: cannot get task table\n");
+        ioctl(fd, MEM_GETMAXTASKS, &maxtasks) < 0 ||
+        ioctl(fd, MEM_GETTASK, &off) < 0) {
+        errmsg("logout: cannot get kernel info\n");
         close(fd);
         return 1;
     }
 
+    /* find login shell: process where uid matches and pid == pgrp */
     for (j = 0; j < maxtasks; j++) {
-        if (memread(fd, off + j*sizeof(struct task_struct), ds,
+        if (memread(fd, off + j * sizeof(struct task_struct), ds,
                 &task_table, sizeof(task_table)) != sizeof(task_table))
             continue;
-
         if (task_table.state == TASK_UNUSED ||
             task_table.state == TASK_ZOMBIE ||
             task_table.state == TASK_EXITING)
             continue;
-
-        if (target_uid == (uid_t)-1) {
-            /* logout self: find our own UID, kill all our TTY processes */
-            if (task_table.uid == getuid() && task_table.tty != 0) {
-                kill(task_table.pid, SIGHUP);
-                found++;
-            }
-        } else {
-            /* logout specific user */
-            if (task_table.uid == target_uid && task_table.tty != 0) {
-                kill(task_table.pid, SIGHUP);
-                found++;
-            }
+        if (task_table.uid == target_uid && task_table.pid == task_table.pgrp) {
+            kill(-task_table.pgrp, SIGHUP);
         }
     }
 
     close(fd);
-
-    if (target_uid != (uid_t)-1 && !found)
-        fprintf(stderr, "logout: no active sessions for '%s'\n", argv[1]);
-
     return 0;
 }
