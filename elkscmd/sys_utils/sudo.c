74b1bba296ce7d1bdb807812127bac46698f1cf3
derekjohansen <derekjohansen1@gmail.com>
2026-07-25 21:35:51 -0600

    fixes from testing

--- elkscmd/sys_utils/sudo.c
+++ elkscmd/sys_utils/sudo.c
@@ -3,30 +3,55 @@
  *
  * Usage: sudo <command> [args...]
  *
+ * Requires caller to be in "wheel" group and know root password.
  * If already root, executes directly.
- * Otherwise prompts for root password and runs command as root.
  */
 
 #include <unistd.h>
 #include <sys/types.h>
 #include <pwd.h>
+#include <grp.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+static int in_wheel_group(uid_t uid)
+{
+    struct group *grp;
+    char **mem;
+
+    grp = getgrnam("wheel");
+    if (!grp) return 0;
+
+    for (mem = grp->gr_mem; *mem; mem++) {
+        struct passwd *pw = getpwnam(*mem);
+        if (pw && pw->pw_uid == uid)
+            return 1;
+    }
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
     struct passwd *pwd;
     char *pbuf, salt[3];
+    uid_t uid;
 
     if (argc < 2) {
         fprintf(stderr, "Usage: sudo <command> [args...]\n");
         return 1;
     }
 
-    if (getuid() == 0) {
+    uid = getuid();
+
+    if (uid == 0) {
         execvp(argv[1], &argv[1]);
         fprintf(stderr, "sudo: %s: command not found\n", argv[1]);
+        return 1;
+    }
+
+    if (!in_wheel_group(uid)) {
+        fprintf(stderr, "sudo: user not in wheel group\n");
         return 1;
     }
 
@@ -37,7 +62,6 @@ int main(int argc, char **argv)
     }
 
     if (pwd->pw_passwd[0] == '\0') {
-        /* no password set for root, just run it */
         setuid(0);
         setgid(0);
         execvp(argv[1], &argv[1]);
@@ -48,7 +72,6 @@ int main(int argc, char **argv)
     pbuf = getpass("Password:");
     if (!pbuf) return 1;
 
-    /* check password - root password must be at least 2 chars for salt */
     if (pwd->pw_passwd[0] && pwd->pw_passwd[1]) {
         salt[0] = pwd->pw_passwd[0];
         salt[1] = pwd->pw_passwd[1];
