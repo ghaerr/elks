723d97a2916cd048a7c3e2167311367f8e7ae92b
Greg Haerr <greg@censoft.com>
2026-07-28 19:08:56 -0700

    Reorder selectors for eventual moving SEL_VIDEO to 0xB800

--- elks/include/arch/segment.h
+++ elks/include/arch/segment.h
@@ -12,14 +12,14 @@
 #define SEL_KDATA       0x10  /* kernel DS = SS */
 #define SEL_KFTEXT      0x18  /* kernel far text (.fartext, medium model) */
 #define SEL_KDATA_EXEC  0x20  /* kernel data aliased as readable CODE (IRQ trampolines) */
-#define SEL_SETUP       0x28  /* setup.S data segment  (SEG_INITSEG, for setupb/setupw) */
-#define SEL_OPTSEG      0x30  /* boot options area (SEG_OPTSEG, /bootopts at init) */
-#define SEL_BIOSDATA    0x38  /* BIOS data area (SEG_BIOSDATA) */
-#define SEL_VIDEO       0x40  /* CGA/EGA/VGA text video memory (SEG_VIDEO) */
-#define SEL_TRACKBUF    0x48  /* directfd DMA track buffer (SEG_TRACK) */
-#define SEL_DMABUF      0x50  /* ATA/CF DMA sector buffer (SEG_DMASEG) */
-#define SEL_IDT         0x58  /* IDT table access at physical address 0:0 */
-#define SEL_GDT         0x60  /* GDT table access (directly follows kernel data segment */
+#define SEL_GDT         0x28  /* GDT table access (directly follows kernel data segment */
+#define SEL_IDT         0x30  /* IDT table access at physical address 0:0 */
+#define SEL_SETUP       0x38  /* setup.S data segment  (SEG_INITSEG, for setupb/setupw) */
+#define SEL_OPTSEG      0x40  /* boot options area (SEG_OPTSEG, /bootopts at init) */
+#define SEL_BIOSDATA    0x48  /* BIOS data area (SEG_BIOSDATA) */
+#define SEL_TRACKBUF    0x50  /* directfd DMA track buffer (SEG_TRACK) */
+#define SEL_DMABUF      0x58  /* ATA/CF DMA sector buffer (SEG_DMASEG) */
+#define SEL_VIDEO       0x60  /* CGA/EGA/VGA text video memory (SEG_VIDEO) */
 #define SEL_FIRST_DYN   0x68  /* first dynamically-allocated selector */
 
 #define GDT_SIZE        ((unsigned)(MAX_GDT_ENTRIES << 3))
